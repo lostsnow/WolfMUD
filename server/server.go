02fd80109fd4583cec6477ae0d67a5035d97034c
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2012-04-10 13:38:45 +0100

    Recreate the Dragon's Breath tavern
    Might as well make the test data interesting. These are the original names,
    descriptions and exits for the Dragon's Breath tavern from the Java WolfMUD.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- server/server.go
+++ server/server.go
@@ -12,21 +12,30 @@ Starting WolfMUD server
 -----------------------`)
 
 	world := [...]location.Location{
-		location.NewBasic("Room one", "You are in the first location."),
-		location.NewBasic("Room two", "You are in the second location."),
-		location.NewBasic("Room three", "You are in the third location."),
+		location.NewBasic("Fireplace", "You are in the corner of a common room in the Dragon's Breath tavern. There is a fire burning away merrily in an ornate fireplace giving comfort to weary travellers. Shadows flicker around the room, changing light to darkness and back again. To the south the common room extends and east the common room leads to the tavern entrance."),
+		location.NewBasic("Common Room", "You are in a small, cosy common room in the Dragon's Breath tavern. Looking around you see a few chairs and tables for patrons. To the east there is a bar and to the north you can see a merry fireplace burning away."),
+		location.NewBasic("Tavern Bar", "You standing at the bar. Behind which you can see various sized and shaped bottles. Looking at the contents you decide an abstract painter would get lots of colourful inspirations after a long night here."),
+		location.NewBasic("Tavern entrance", "You are in the entryway to the Dragon's Breath tavern. To the west you can see an inviting fireplace, while south an even more inviting bar. Eastward a door leads out into the street."),
 	}
 
-	world[0].SetExit(location.N, world[1])
+	// Fireplace
+	world[0].SetExit(location.E, world[3])
+	world[0].SetExit(location.S, world[1])
 	world[0].SetExit(location.SE, world[2])
-	world[1].SetExit(location.SOUTH, world[0])
 
-	l := world[0]
-	l.Look()
-	l = l.Move(location.N)
-	l = l.Move(location.NORTH)
-	l = l.Move(location.S)
-	l = l.Move(location.SOUTH)
-	l = l.Move(location.SE)
+	// Common room
+	world[1].SetExit(location.N, world[0])
+	world[1].SetExit(location.E, world[2])
+	world[1].SetExit(location.NE, world[3])
+
+	// Tavern Bar
+	world[2].SetExit(location.N, world[3])
+	world[2].SetExit(location.W, world[1])
+	world[2].SetExit(location.NW, world[0])
+
+	// Tavern Entrance
+	world[3].SetExit(location.S, world[2])
+	world[3].SetExit(location.W, world[0])
+	world[3].SetExit(location.SW, world[1])
 
 }
