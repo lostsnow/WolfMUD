cb632ffe06f7938cc4a5c42c01412af192d47456
Andrew Rolfe <andrew@bulldogdigital.co.uk>
2012-04-17 21:14:49 +0100

    As we are only handling single aliases at the moment add a shortcut to inventory.delegate()
    Signed-off-by: Andrew Rolfe <andrew@bulldogdigital.co.uk>

--- entities/inventory.go
+++ entities/inventory.go
@@ -21,21 +21,22 @@ func NewInventory() Inventory {
 }
 
 func (i *inventory) delegate(what Thing, cmd string, args []string) (handled bool) {
+	if len(args[0]) == 0 {
+		return false
+	}
+
 	// An inventory delegates to everything in it but handles nothing itself
-ABORT:
-	for _, alias := range i.content {
-		for _, object := range alias {
+	for _, object := range i.content[args[0]] {
 
-			// Don't process ourself at a location - gets recursive!
-			if what == object {
-				continue
-			}
+		// Don't process ourself at a location - gets recursive!
+		if what == object {
+			continue
+		}
 
-			if _, ok := object.(Commander); ok {
-				handled = object.Command(what, cmd, args)
-				if handled {
-					break ABORT
-				}
+		if _, ok := object.(Commander); ok {
+			handled = object.Command(what, cmd, args)
+			if handled {
+				break
 			}
 		}
 	}
