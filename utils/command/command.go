934bc74c54ac06368580ce6fdbb5864e046c81f2
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2012-05-31 21:20:56 +0100

    Documented and improved AddLock
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- utils/command/command.go
+++ utils/command/command.go
@@ -103,7 +103,7 @@ func (c *Command) LocksModified() (modified bool) {
 	return
 }
 
-// AddLock adds a reference to a thing and adds it to the Locks slice in the
+// AddLock takes a reference to a thing and adds it to the Locks slice in the
 // correct position. Locks should always be acquired in unique Id sequence
 // lowest to highest to avoid deadlocks. By using this method the Locks property
 // can easily be iterated via a range and in the correct sequence required.
@@ -111,7 +111,33 @@ func (c *Command) LocksModified() (modified bool) {
 // NOTE: We cannot add the same Lock twice otherwise we would deadlock ourself
 // when locking.
 //
-// BUG(D) Should really implement sort interface?
+// This routine is a little cute and avoids doing any 'real' sorting to keep the
+// elements in unique ID sequence. We add our lock to our slice. If we have one
+// element only it's what we just added so we bail.
+//
+// If we have multiple elements we have the appended element on the end and need
+// to check where it goes, shift the trailing element up by one the write our
+// new element in:
+//
+//	3 7 9 4 <- append new element to end
+//	3 7 9 4 <- correct place: 4 goes between 3 and 7
+//	3 7 7 9 <- shift 7,9 up one overwriting our appended element
+//	3 4 7 9 <- we now write our new element into our 'hole'
+//
+// What if we can't find an element with a unique Id greater than the one we are
+// inserting?
+//
+//	3 7 9 10 <- append new element to end
+//	3 7 9 10 <- correct place: 10 goes after 9, not insert point found
+//	3 7 9 10 <- No shifting is done, appended element not over-written
+//	3 4 7 10 <- new element already in correct place, nothing else to do
+//
+// This function could be more efficient with large numbers of elements by
+// using a binary search to find the insertion point for the new element.
+// However this would make the code more complex and we don't expect huge
+// numbers of elements.
+//
+// Amazing, 347 characters of code, 1696 characters of comments!
 func (c *Command) AddLock(t thing.Interface) {
 
 	if t == nil || c.CanLock(t) {
@@ -119,17 +145,15 @@ func (c *Command) AddLock(t thing.Interface) {
 	}
 
 	c.locksModified = true
-
 	c.Locks = append(c.Locks, t)
-	if len(c.Locks) == 1 {
-		return
-	}
-	for swap := true; swap; {
-		swap = false
-		for i := len(c.Locks) - 2; i >= 0; i-- {
-			if c.Locks[i].UniqueId() > c.Locks[i+1].UniqueId() {
-				c.Locks[i], c.Locks[i+1] = c.Locks[i+1], c.Locks[i]
-				swap = true
+
+	if l := len(c.Locks); l > 1 {
+		uid := t.UniqueId()
+		for i := 0; i < l; i++ {
+			if uid > c.Locks[i].UniqueId() {
+				copy(c.Locks[i+1:l], c.Locks[i:l-1])
+				c.Locks[i] = t
+				break
 			}
 		}
 	}
