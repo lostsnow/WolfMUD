25bf2ef0e091e7ca8600e4be8fe79bd951e0094c
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2012-10-31 16:46:50 +0000

    player: Improved documentation in playerList
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- entities/mobile/player/playerList.go
+++ entities/mobile/player/playerList.go
@@ -1,3 +1,8 @@
+// Copyright 2012 Andrew 'Diddymus' Rolfe. All rights reserved.
+//
+// Use of this source code is governed by the license in the LICENSE file
+// included with the source code.
+
 package player
 
 import (
@@ -7,33 +12,40 @@ import (
 	"wolfmud.org/utils/text"
 )
 
+// playerList type records the current players on the server.
 type playerList struct {
 	players []*Player
 	mutex   chan bool
 }
 
+// PlayerList exports the playerList type
 var (
 	PlayerList playerList
 )
 
+// Function init makes the mutex channel for locking.
 func init() {
 	PlayerList.mutex = make(chan bool, 1)
 }
 
+// lock acquires a lock on a playerList reference.
 func (l *playerList) lock() {
 	l.mutex <- true
 }
 
+// unlock releases a lock on a playerList reference.
 func (l *playerList) unlock() {
 	<-l.mutex
 }
 
+// Add adds a player to the playerList
 func (l *playerList) Add(player *Player) {
 	l.lock()
 	defer l.unlock()
 	l.players = append(l.players, player)
 }
 
+// Remove removes a player from the playerList
 func (l *playerList) Remove(player *Player) {
 	l.lock()
 	defer l.unlock()
@@ -50,6 +62,7 @@ func (l *playerList) Remove(player *Player) {
 	}
 }
 
+// Length returns the number of players in the playerList
 func (l *playerList) Length() int {
 	l.lock()
 	defer l.unlock()
@@ -63,6 +76,18 @@ func (l *playerList) List(omit ...thing.Interface) (list []*Player) {
   return l.nonLockingList(omit...)
 }
 
+// Broadcast implements the broadcaster interface and sends a message to all
+// players currently on the server. The omit parameter specifies things not to
+// send the message to. For example if we had a scream command we might send a
+// message to everyone else:
+//
+//	You hear someone scream.
+//
+// The message you would see might be:
+//
+//	You scream!
+//
+// However you would not want to see the 'You hear someone scream' message.
 func (l *playerList) Broadcast(omit []thing.Interface, format string, any ...interface{}) {
 	l.lock()
 	defer l.unlock()
@@ -74,6 +99,12 @@ func (l *playerList) Broadcast(omit []thing.Interface, format string, any ...int
 	}
 }
 
+// nonLockingList returns the current players on the server with possible
+// omissions. This method is non-locking so that other locking methods can
+// call it. If this method also locked we would end up deadlocking. The omit
+// parameter specifies any things to be omitted from the returned list - handy
+// when a player wants to know who else is on the server and not including
+// themselves for example.
 func (l *playerList) nonLockingList(omit ...thing.Interface) (list []*Player) {
 
 OMIT:
