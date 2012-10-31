8308dca0d9fbce93bd87d663613856d820366e89
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2012-10-31 14:07:37 +0000

    loader: Added locations 16-42
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- utils/loader/loader.go
+++ utils/loader/loader.go
@@ -49,6 +49,60 @@ func Load(world *world.World) {
 
 	l15 := location.New("Street outside Trading Post", []string{"STREET", "TRADINGPOST"}, "You are on a street running east to west. To north is a large Trading Post.")
 
+	l16 := location.New("Trading Post", []string{"TRADINGPOST", "SHOP"}, "You are standing in a large Trading Post . The only exit is west into the street.")
+
+	l17 := location.New("Street outside Guard House", []string{"STREET", "GUARDHOUSE"}, "You are on a street running east to west. To north is the cities Guard House.")
+
+	l18 := location.New("Guard House", []string{"GUARDHOUSE", "STREET"}, "You are in the cities Guard House.")
+
+	l19 := location.New("Street by North Bridge", []string{"STREET"}, "You are at a junction in the street. You can either head south, east or west. East there is the north bridge over the cities river.")
+
+	l20 := location.New("North Bridge", []string{"NORTHBRIDGE"}, "You are standing on the west side of an incomplete bridge. By the looks of it the city wants to expand onto the far banks of the river. Down river to the south you can see another bridge in a similar state of construction.")
+
+	l21 := location.New("Street by South Bridge", []string{"STREET"}, "You are at a junction in the street. You can either head north, east or west. East there is the south bridge over the cities river.")
+
+	l22 := location.New("South Bridge", []string{"SOUTHBRIDGE"}, "You are standing on the west side of an incomplete bridge. By the looks of it the city wants to expand onto the far banks of the river. Up river to the north you can see another bridge in a similar state of construction.")
+
+	l23 := location.New("Money Changer's Office", []string{"MONEYCHANGER", "SHOP"}, "You are standing in the small office of a Money Changer. The only exit is north into the street.")
+
+	l24 := location.New("Street outside Money Changer", []string{"STREET"}, "You are in the street outside a small Money Changer's Office. The street heads to the east and west. The entrance to the Money Changer is to the South.")
+
+	l25 := location.New("Street", []string{"STREET"}, "You are in the street which is running east and west. To the north looms a dark alley.")
+
+	l26 := location.New("Street outside City Gardens", []string{"STREET"}, "You are at the end of street which runs to the east. To the west you can see the entrance to the City Gardens. A narrow alley runs south.")
+
+	l27 := location.New("Entrance to City Gardens", []string{"GARDENS", "ENTRANCE"}, "You are standing at the entrance to the City Gardens. Around you flowers blossom and insects hum. West you can see an intricate iron gate leading into the gardens. East the streets await.")
+
+	l28 := location.New("Gardens", []string{"GARDENS"}, "You are in a fine, formal garden. Around you small trees and shrubs grow. Here and there a splash of colour is present in the form of small delicate flowers. To the east you can see the gateway, northward there is a fish pond.")
+
+	l29 := location.New("Quiet Sheltered Area", []string{"GARDENS", "SHELTERED"}, "You are in a small quiet area of the garden. Tall bushes have grown up over a trellis work to provide a small shaded area to come and sit in. The only exit from this area is southward.")
+
+	l30 := location.New("Gardens", []string{"GARDENS"}, "You are in a fine, formal garden. Around you small trees and shrubs grow. Here and there a splash of colour is present in the form of small delicate flowers. The gardens continue north, south and west.")
+
+	l31 := location.New("Fishpond", []string{"GARDENS", "FISHPOND"}, "You are standing next to a small fishpond. Paths lead off north, south and west deeper into the gardens.")
+
+	l32 := location.New("Gardens", []string{"GARDENS"}, "You are in a fine, formal garden. Around you small trees and shrubs grow. Here and there a splash of colour is present in the form of small delicate flowers. The gardens continue north, south and east.")
+
+	l33 := location.New("Gardens", []string{"GARDENS"}, "You are in a fine, formal garden. Around you small trees and shrubs grow. Here and there a splash of colour is present in the form of small delicate flowers. The gardens continue east, west and south.")
+
+	l34 := location.New("Quiet Sheltered Area", []string{"GARDENS", "SHELTERED"}, "You are in a small quiet area of the garden. Tall bushes have grown up over a trellis work to provide a small shaded area to come and sit in. The only exit from this area is eastward.")
+
+	l35 := location.New("Gravel Path", []string{"GARDENS", "PATH"}, "You find yourself on a narrow gravel path leading between some bushes. The path continues south or you can go north into the gardens.")
+
+	l36 := location.New("Secluded Path by Shed", []string{"GARDENS", "PATH", "SECLUDED"}, "You are on a small secluded gravel path screened off from the formal gardens by some large bushes. To the south you can make out the door to a small shed. A rock has been positioned by the shed, from the looks of it by someone who thought it might be artistic. The gravel path you are on leads northward.")
+
+	l37 := location.New("Garden Shed", []string{"GARDENS", "SHED"}, "You are in a small garden shed. The only exit appears to be through the door to the north.")
+
+	l38 := location.New("Dim Alley", []string{"ALLEY"}, "You are in a dim alley full of rubbish. The alley continues northward. To the south it leads into the street.")
+
+	l39 := location.New("Dim Alley", []string{"ALLEY"}, "You are in a dim alley full of rubbish. The alley continues north and south.")
+
+	l40 := location.New("Bend in Dim Alley", []string{"ALLEY"}, "You are in a dim alley full of rubbish. Here the alley bends to the south and to the west.")
+
+	l41 := location.New("Rogue's Den Entrance", []string{"DEN", "ENTRANCE"}, "You are in a dim alley which leads to the east. Partially hidden by rubbish you can see a small trap door.")
+
+	l42 := location.New("Rogue's Den", []string{"DEN"}, "You are in a large dim room. Looking around to catch glimpses of things moving in the shadows.")
+
 	// Fireplace
 	l1.LinkExit(location.E, l3)
 	l1.LinkExit(location.SE, l4)
@@ -95,7 +149,7 @@ func Load(world *world.World) {
 	// Street outside Armourer
 	l10.LinkExit(location.E, l9)
 	l10.LinkExit(location.S, l11)
-	//W24
+	l10.LinkExit(location.W, l24)
 
 	// Armourer's
 	l11.LinkExit(location.N, l10)
@@ -103,7 +157,7 @@ func Load(world *world.World) {
 	// Street outside Weapon Shop
 	l12.LinkExit(location.S, l13)
 	l12.LinkExit(location.W, l9)
-	//E21
+	l12.LinkExit(location.E, l21)
 
 	// Weapons Shop
 	l13.LinkExit(location.N, l12)
@@ -111,13 +165,122 @@ func Load(world *world.World) {
 	// Crossroads
 	l14.LinkExit(location.E, l15)
 	l14.LinkExit(location.S, l5)
-	// S17
+	l14.LinkExit(location.W, l17)
 	// N45
 
 	// Street outside Trading Post
 	l15.LinkExit(location.W, l14)
-	// N16
-	// E19
+	l15.LinkExit(location.N, l16)
+	l15.LinkExit(location.E, l19)
+
+	// Trading Post
+	l16.LinkExit(location.S, l15)
+
+	// Street outside Guard House
+	l17.LinkExit(location.N, l18)
+	l17.LinkExit(location.E, l14)
+	l17.LinkExit(location.W, l25)
+
+	// Guard House
+	l18.LinkExit(location.S, l17)
+
+	// Street by North Bridge
+	l19.LinkExit(location.E, l20)
+	l19.LinkExit(location.W, l15)
+	// S43
+
+	// North Bridge
+	l20.LinkExit(location.W, l19)
+
+	// Street by South Bridge
+	l21.LinkExit(location.E, l22)
+	l21.LinkExit(location.W, l12)
+	// N44
+
+	// South Bridge
+	l22.LinkExit(location.W, l21)
+
+	// Money Changer's Office
+	l23.LinkExit(location.N, l24)
+
+	// Street outside Money Changer
+	l24.LinkExit(location.E, l10)
+	l24.LinkExit(location.S, l23)
+	// W60
+
+	// Street
+	l25.LinkExit(location.E, l17)
+	l25.LinkExit(location.W, l26)
+	l25.LinkExit(location.N, l38)
+
+	// Street outside City Gardens
+	l26.LinkExit(location.E, l25)
+	l26.LinkExit(location.W, l27)
+	// S58
+
+	// Entrance to City Gardens
+	l27.LinkExit(location.E, l26)
+	l27.LinkExit(location.W, l28)
+
+	// Gardens
+	l28.LinkExit(location.E, l27)
+	l28.LinkExit(location.N, l31)
+
+	// Quiet Sheltered Area
+	l29.LinkExit(location.S, l30)
+
+	// Gardens
+	l30.LinkExit(location.N, l29)
+	l30.LinkExit(location.S, l31)
+	l30.LinkExit(location.W, l33)
+
+	// Fishpond
+	l31.LinkExit(location.N, l30)
+	l31.LinkExit(location.S, l28)
+	l31.LinkExit(location.W, l32)
+
+	// Gardens
+	l32.LinkExit(location.N, l33)
+	l32.LinkExit(location.E, l31)
+	l32.LinkExit(location.S, l35)
+
+	// Gardens
+	l33.LinkExit(location.E, l30)
+	l33.LinkExit(location.S, l32)
+	l33.LinkExit(location.W, l34)
+
+	// Quiet Sheltered Area
+	l34.LinkExit(location.E, l33)
+
+	// Gravel Path
+	l35.LinkExit(location.N, l32)
+	l35.LinkExit(location.S, l36)
+
+	// Secluded Path by Shed
+	l36.LinkExit(location.N, l35)
+	l36.LinkExit(location.S, l37)
+
+	// Garden Shed
+	l37.LinkExit(location.N, l36)
+
+	// Dim Alley
+	l38.LinkExit(location.N, l39)
+	l38.LinkExit(location.S, l25)
+
+	// Dim Alley
+	l39.LinkExit(location.N, l40)
+	l39.LinkExit(location.S, l38)
+
+	// Bend in Dim Alley
+	l40.LinkExit(location.S, l39)
+	l40.LinkExit(location.W, l41)
+
+	// Rogue's Den Entrance
+	l41.LinkExit(location.E, l40)
+	l41.LinkExit(location.D, l42)
+
+	// Rogue's Den
+	l42.LinkExit(location.U, l41)
 
 	world.AddLocation(l1)
 	world.AddLocation(l2)
@@ -134,6 +297,33 @@ func Load(world *world.World) {
 	world.AddLocation(l13)
 	world.AddLocation(l14)
 	world.AddLocation(l15)
+	world.AddLocation(l16)
+	world.AddLocation(l17)
+	world.AddLocation(l18)
+	world.AddLocation(l19)
+	world.AddLocation(l20)
+	world.AddLocation(l21)
+	world.AddLocation(l22)
+	world.AddLocation(l23)
+	world.AddLocation(l24)
+	world.AddLocation(l25)
+	world.AddLocation(l26)
+	world.AddLocation(l27)
+	world.AddLocation(l28)
+	world.AddLocation(l29)
+	world.AddLocation(l30)
+	world.AddLocation(l31)
+	world.AddLocation(l32)
+	world.AddLocation(l33)
+	world.AddLocation(l34)
+	world.AddLocation(l35)
+	world.AddLocation(l36)
+	world.AddLocation(l37)
+	world.AddLocation(l38)
+	world.AddLocation(l39)
+	world.AddLocation(l40)
+	world.AddLocation(l41)
+	world.AddLocation(l42)
 
 	// Some objects
 	t1 := thing.New(
