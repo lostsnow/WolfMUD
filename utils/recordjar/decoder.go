cd500278df50c51e561519885ceca9df454df675
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2013-04-07 22:46:33 +0100

    recordjar: New recordjar package
    The recordjar package is used for reading (and soon writing) WolfMUD data
    files. This package needs to be rejigged a little to make the decoder - and
    soon to be added encoder - easier and more automatic.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

---
+++ utils/recordjar/decoder.go
@@ -0,0 +1,69 @@
+// Copyright 2013 Andrew 'Diddymus' Rolfe. All rights reserved.
+//
+// Use of this source code is governed by the license in the LICENSE file
+// included with the source code.
+
+package recordjar
+
+import (
+	"log"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// String is a helper that returns the value of a header from a Record as a
+// string. If the header is not found in the Record an empty string is returned.
+func (r Record) String(property string) string {
+	if _, ok := r[property]; !ok {
+		log.Printf("Property %q not found. Defaulting to empty string", property)
+		return ""
+	}
+	return strings.TrimSpace(r[property])
+}
+
+// KeywordList is a helper that returns the value of a header from a Record
+// interpreted as whitespace separated keywords. It returns the keywords as a
+// slice of uppercased strings. If the header is not found in the Record an
+// empty string slice is returned.
+func (r Record) KeywordList(property string) []string {
+	if _, ok := r[property]; !ok {
+		log.Printf("Property %q not found. Defaulting to empty list", property)
+		return []string{}
+	}
+	return strings.Fields(strings.ToUpper(r[property]))
+}
+
+// Int is a helper that returns the value of a header from a Record interpreted
+// - as parsed by strconv.Atoi - as an integer. If the header is not found
+// in the Record or the value cannot be parsed as an integer integer zero is
+// returned.
+func (r Record) Int(property string) (i int) {
+	if _, ok := r[property]; !ok {
+		log.Printf("Property %q not found. Defaulting to zero", property)
+		return 0
+	}
+	var err error
+	if i, err = strconv.Atoi(r[property]); err != nil {
+		log.Printf("Error retrieving %q as type int: %s. Defaulting to zero.", r[property], err)
+		return 0
+	}
+	return i
+}
+
+// Duration is helper that returns the value of a header from a Record
+// interpreted - as parsed by time.ParseDuration - as a duration of time. If the
+// header is not found in the Record or the value cannot be parsed as a duration
+// a zero duration is returned.
+func (r Record) Duration(property string) (d time.Duration) {
+	if _, ok := r[property]; !ok {
+		log.Printf("Property %q not found. Defaulting to zero", property)
+		return 0
+	}
+	var err error
+	if d, err = time.ParseDuration(r[property]); err != nil {
+		log.Printf("Error parsing %q as type time.Duration: %s. Defaulting to zero.", r[property], err)
+		return 0
+	}
+	return d
+}
---
+++ utils/recordjar/recordjar.go
@@ -0,0 +1,167 @@
+// Copyright 2013 Andrew 'Diddymus' Rolfe. All rights reserved.
+//
+// Use of this source code is governed by the license in the LICENSE file
+// included with the source code.
+
+// Package recordjar implements the main file format used by WolfMUD.  It is
+// based on a combination of RFC5322 and the Cookie Jar format as described by
+// Eric Raymond in "The Art of Unix Programming", chapter 5:
+//
+//	http://www.catb.org/esr/writings/taoup/html/ch05s02.html
+//
+// It is not an actual implementation of the RFC5322 format just based on it:
+//
+//	- Unicode is allowed in header names and values
+//	- Whitespace handling is more lenient and may proceed header names
+//	- Line endings can be CRLF or LF
+//	- Comments are lines starting with '//' characters
+//	- Multiple records are separated by the '%%' sequence
+//
+// Here is a simple example of two starting locations:
+//
+//		//
+//		// The Dragon's Breath tavern. L1 to L4
+//		//
+//				Ref: L1
+//			 Type: Start
+//			 Name: Fireplace
+//		Aliases: TAVERN FIREPLACE
+//			Exits: E→L3 SE→L4 S→L2
+//
+//		You are in the corner of a common room in the Dragon's Breath tavern.
+//		There is a fire burning away merrily in an ornate fireplace giving
+//		comfort to weary travellers. Shadows flicker around the room, changing
+//		light to darkness and back again. To the south the common room extends
+//		and east the common room leads to the tavern entrance.
+//		%%
+//				Ref: L2
+//			 Type: Start
+//			 Name: Common Room
+//		Aliases: TAVERN COMMON
+//			Exits: N→L1 NE→L3 E→L4
+//
+//		You are in a small, cosy common room in the Dragon's Breath tavern.
+//		Looking around you see a few chairs and tables for patrons. To the east
+//		there is a bar and to the north you can see a merry fireplace burning
+//		away.
+//
+//
+// When this example is read you would have a RecordJar which is a slice of
+// Records - in this case two of them.
+package recordjar
+
+import (
+	"bufio"
+	"io"
+	"regexp"
+	"strings"
+)
+
+// Record represents a section read from a record jar file. A Record is a map of
+// strings keyed by header strings.
+type Record map[string]string
+
+// RecordJar is a slice of Records. When a file is read a RecordJar contains all
+// of the Records from the file.
+type RecordJar []Record
+
+// Unmarshaler should be implemented by any type that can take data represented
+// by a Record and parse / decode it.
+type Unmarshaler interface {
+	Unmarshal(Record)
+}
+
+// Constants for header line types
+const (
+	headerSeparator = ""
+	recordSeparator = "%%"
+	comment         = "//"
+)
+
+// splitHeader is a regexp to split the header prefix from a line
+var (
+	splitHeader = regexp.MustCompile(`^(?:([^\s:]+):)?\s*(.*?)$`)
+)
+
+// Read reads data from the passed io.Reader and returns a RecordJar of Records.
+// If there is an error returns a nil RecordJar and the error.
+//
+// TODO: Need to detail specifics of RecordJar, Record and file format.
+func Read(reader io.Reader) (rj RecordJar, err error) {
+
+	b := bufio.NewReader(reader)
+	r := make(Record)
+
+	currentHeader := ""
+	line := ""
+
+RECORDS:
+	for {
+
+		// If we have a record on the go store it and allocate a new one
+		if len(r) != 0 {
+			rj = append(rj, r)
+			r = make(Record)
+		}
+
+		// Exit at EOF
+		if err == io.EOF {
+			break RECORDS
+		}
+
+		// Process record header lines
+	HEADERS:
+		for {
+			line, err = b.ReadString('\n')
+			line = strings.TrimSpace(line)
+
+			switch {
+			case line == headerSeparator:
+				break HEADERS
+			case line == recordSeparator:
+				continue RECORDS
+			case len(line) > 1 && line[0:2] == comment:
+				continue HEADERS
+			}
+
+			tokens := splitHeader.FindStringSubmatch(line)
+			newHeader, data := strings.ToLower(tokens[1]), tokens[2]
+
+			if newHeader != "" {
+				currentHeader = newHeader
+			}
+
+			if _, ok := r[currentHeader]; ok {
+				r[currentHeader] += " "
+			}
+			r[currentHeader] += data
+
+			if err != nil {
+				if err != io.EOF {
+					return nil, err
+				}
+				break RECORDS
+			}
+		}
+
+		// Process free data lines - between header separator and record separator / EOF
+		for {
+			line, err = b.ReadString('\n')
+
+			if line == recordSeparator+"\n" || (line == "" && err == io.EOF) {
+				continue RECORDS
+			}
+
+			r[":data:"] += line
+
+			if err != nil {
+				if err != io.EOF {
+					return nil, err
+				}
+				break RECORDS
+			}
+		}
+	}
+
+	return rj, nil
+}
