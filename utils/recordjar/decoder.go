21f6d67d81a937caa16d31f3eddea58946d65ee4
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2013-09-17 20:42:05 +0100

    recordjar: Added decoder.Keyword helper method
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- utils/recordjar/decoder.go
+++ utils/recordjar/decoder.go
@@ -22,6 +22,16 @@ func (r Record) String(property string) string {
 	return strings.TrimSpace(r[property])
 }
 
+// Keyword is a helper that returns the value of a header from a Record as an
+// UPPERCASED string. If the header is not found in the Record an empty string
+// is returned.
+//
+// This function is helpful for Ids and references which are case insensative
+// and for consistency when matching are usually uppercased.
+func (r Record) Keyword(property string) string {
+	return strings.ToUpper(r.String(property))
+}
+
 // KeywordList is a helper that returns the value of a header from a Record
 // interpreted as whitespace separated keywords. It returns the keywords as a
 // slice of uppercased strings. If the header is not found in the Record an
