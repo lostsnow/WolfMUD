2341e5a448a1113b051361cafaa4b14fdd867393
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2015-02-03 10:43:04 +0000

    attr: All FindAll method to description
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- attr/description.go
+++ attr/description.go
@@ -37,6 +37,15 @@ func (*description) Find(t has.Thing) has.Description {
 	return nil
 }
 
+func (*description) FindAll(t has.Thing) (matches []has.Description) {
+	for _, a := range t.Attrs() {
+		if a, ok := a.(has.Description); ok {
+			matches = append(matches, a)
+		}
+	}
+	return
+}
+
 func (d *description) Dump() []string {
 	return []string{DumpFmt("%p %[1]T %q", d, d.description)}
 }
