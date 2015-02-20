8e3bfc40b9751d193ec3f991bb23e30b970792ab
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2015-02-20 21:46:42 +0000

    cmd: Drop unused utils.what function
    This function didn't really work as well as intended. Frequently access was
    needed to the inventories after calling what which meant a second attribute
    lookup to find it in the calling code. This affected performance quite a bit.
    The equivalent of what has now been inlined in the relevant commands. This has
    two benefits: Inventories looked up can be reused and the sequence that
    inventories and narratives are visited can be varied depending on the command
    and it's requirements.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- cmd/utils.go
+++ cmd/utils.go
@@ -10,40 +10,6 @@ import (
 	"code.wolfmud.org/WolfMUD-mini.git/has"
 )
 
-func what(alias string, t has.Thing) (what has.Thing) {
-
-	// If thing knows it's in an inventory try that inventory first
-	if a := attr.Locate().Find(t); a != nil {
-		if what = search(alias, a.Where()); what != nil {
-			return what
-		}
-	}
-
-	// If we haven't found our what and where yet check our thing's inventory
-	if what = search(alias, t); what != nil {
-		return what
-	}
-
-	// 404 - Not found :(
-	return nil
-}
-
-func search(alias string, t has.Thing) (what has.Thing) {
-	if a := attr.Inventory().Find(t); a != nil {
-		if what = a.Search(alias); what != nil {
-			return
-		}
-	}
-
-	if a := attr.Narrative().Find(t); a != nil {
-		if what = a.Search(alias); what != nil {
-			return
-		}
-	}
-
-	return nil
-}
-
 func CheckVetoes(cmd string, what has.Thing) has.Veto {
 	if vetoes := attr.Vetoes().Find(what); vetoes != nil {
 		if veto := vetoes.Check(cmd); veto != nil {
