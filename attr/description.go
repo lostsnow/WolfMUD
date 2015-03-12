965031052bb43e97d6a83c913962af7821eb4f5b
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2015-03-12 20:03:23 +0000

    attr: Document Description
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- attr/description.go
+++ attr/description.go
@@ -9,20 +9,29 @@ import (
 	"code.wolfmud.org/WolfMUD-mini.git/has"
 )
 
+// Description implements an attribute for describing Things. Things can have
+// multiple descriptions or other attributes that implement the has.Description
+// interface to add additional information to descriptions.
 type Description struct {
 	Attribute
 	description string
 }
 
-// Some interfaces we want to make sure we implement
+// Some interfaces we want to make sure we implement. If we don't we'll throw
+// compile time errors.
 var (
 	_ has.Description = &Description{}
 )
 
+// NewDescription returns a new Description attribute initialised with the
+// specified description.
 func NewDescription(description string) *Description {
 	return &Description{Attribute{}, description}
 }
 
+// FindAllDescription searches the attributes of the specified Thing for
+// attributes that implement has.Description returning all that match. If no
+// matches are found an empty slice will be returned.
 func FindAllDescription(t has.Thing) (matches []has.Description) {
 	for _, a := range t.Attrs() {
 		if a, ok := a.(has.Description); ok {
@@ -36,6 +45,7 @@ func (d *Description) Dump() []string {
 	return []string{DumpFmt("%p %[1]T %q", d, d.description)}
 }
 
+// Description returns the descriptive string of the attribute.
 func (d *Description) Description() string {
 	return d.description
 }
