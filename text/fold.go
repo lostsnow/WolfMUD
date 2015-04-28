146ef2df8b57ae88c963a085d82fca50aba5dccf
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2015-04-28 10:19:30 +0100

    text: Improve readability of Fold source
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- text/fold.go
+++ text/fold.go
@@ -9,62 +9,82 @@ import (
 	"bytes"
 )
 
-const space = 1
+const (
+	reset = 0
+	space = 1
+)
 
+// Fold takes a string and reformats it so lines have a maximum length of
+// width. Fold will handle multibyte runes. However it cannot handle 'wide'
+// runes - those that are wider than a normal single character when displayed.
+// This is because the required information is actually contained in the font
+// files of the font in use.
+//
+// For example the Chinese for 9 is 九 (U+4E5D). Even in a monospaced font 九
+// will take up the space of two columns.
 func Fold(in string, width int) []byte {
 
-	// Can we take a short cut?
+	// Can we take a short cut? Note we are just counting bytes here.
 	if len(in) <= width {
 		return []byte(in)
 	}
 
 	// Add extra line feed to end of input. Will cause final word and line to be
 	// 'flushed' from the buffers. The extra line feed itself will not be output
-	// becuase it will still be in the buffers - so we don't need to trim it off.
+	// because it will still be in the buffers - so we don't need to trim it off.
 	in += "\n"
 
-	// Setup some buffers:
-	//	bw: word buffer	  - initially up to 32 (arbitrary) characters
-	//	bl: line buffer		- initially width + 1 word
-	//	bo: output buffer - initially input length + 32 (arbitrary) line breaks
 	var (
-		bw = bytes.NewBuffer(make([]byte, 0, 32))
-		bl = bytes.NewBuffer(make([]byte, 0, width+32))
-		bo = bytes.NewBuffer(make([]byte, 0, len(in)+32))
+		word = bytes.NewBuffer(make([]byte, 0, 32))
+		line = bytes.NewBuffer(make([]byte, 0, width+32))
+		page = bytes.NewBuffer(make([]byte, 0, len(in)+24))
 	)
 
-	lb := true // Only leading blanks have been written to a word
+	var (
+		wordLen, lineLen, pageLen = 0, 0, 0 // word, line and output length in runes
+		blank                     = true    // true when line is empty or only blanks
+	)
 
 	for _, r := range in {
 
-		if (r != ' ' && r != '\n') || (r == ' ' && lb == true) {
-			bw.WriteRune(r)
-			lb = !(r != ' ')
+		if (r != ' ' && r != '\n') || (r == ' ' && blank == true) {
+			word.WriteRune(r)
+			wordLen++
+			blank = r == ' '
 			continue
 		}
 
-		if bl.Len()+space+bw.Len() >= width {
-			if bo.Len() != 0 {
-				bo.WriteByte('\n')
+		if lineLen+space+wordLen > width {
+			if pageLen != reset {
+				page.WriteByte('\n')
+				pageLen++
 			}
-			bl.WriteTo(bo)
-			lb = true
+			line.WriteTo(page)
+			pageLen += lineLen
+			lineLen = reset
+			blank = true
 		}
 
-		if bl.Len() != 0 {
-			bl.WriteByte(' ')
+		if lineLen != reset {
+			line.WriteByte(' ')
+			lineLen++
 		}
-		bw.WriteTo(bl)
+		word.WriteTo(line)
+		lineLen += wordLen
+		wordLen = reset
 
 		if r == '\n' {
-			if bo.Len() != 0 {
-				bo.WriteByte('\n')
+			if pageLen != reset {
+				page.WriteByte('\n')
+				pageLen++
 			}
-			bl.WriteTo(bo)
-			lb = true
+			line.WriteTo(page)
+			pageLen += lineLen
+			lineLen = reset
+			blank = true
 		}
 
 	}
 
-	return bo.Bytes()
+	return page.Bytes()
 }
