9c98111fabd8e9655daf8ca43ab7a3f5511c0c71
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2015-05-27 21:23:46 +0100

    attr: Document attr package
    This definitely needs fleshing out at some point.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

---
+++ attr/doc.go
@@ -0,0 +1,11 @@
+// Copyright 2015 Andrew 'Diddymus' Rolfe. All rights reserved.
+//
+// Use of this source code is governed by the license in the LICENSE file
+// included with the source code.
+
+// The attr (attributes) package implements all of the functionality for objects
+// in a WolfMUD world. All objects are instances of the Thing type. To these
+// instances various attributes are added depending on the functionality
+// required. The functionality of an object may be changed at runtime by adding
+// or removing attributes.
+package attr
