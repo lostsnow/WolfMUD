48fc527030b60c34f512ccc82ce7ca849486ff70
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2015-11-18 21:05:49 +0000

    cmd: Switch parse to use new state
    Switch parser to use the new state type. Note that commands have not been
    updated to use the new state type yet. To work around this in the dispatch
    function we directly interact with the state to extract the data commands are
    expecting and to push data back into the state from the commands. This is only
    temporary and as the commands are updated the dispatch function will be
    cleaned up.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- cmd/parse.go
+++ cmd/parse.go
@@ -9,44 +9,79 @@ import (
 	"code.wolfmud.org/WolfMUD.git/has"
 
 	"strings"
+	"unicode"
 )
 
+// Parse initiates processing of the input string for the specified Thing. The
+// input string is expected to be either a players input or possibly a scripted
+// command. It returns msg which is the response to carry out the command and
+// ok which is set to true if the command completed successfully else false.
+// The ok flag can be used by other commands and possibly scripting to check if
+// the command was successful without having to try and parse the msg.
 func Parse(t has.Thing, input string) (msg string, ok bool) {
-	input = strings.ToUpper(input)
-	words := strings.Fields(input)
 
-	if len(words) == 0 {
+	if strings.TrimLeftFunc(input, unicode.IsSpace) == "" {
 		return
 	}
 
-	cmd := words[0]
+	s := NewState(t, input)
+	s.parse(dispatch)
+
+	return s.msg.actor.String(), s.ok
+}
+
+func dispatch(s *state) {
+
+	var (
+		msg string
+		ok  bool
+
+		cmd   = s.cmd
+		words = s.words
+		t     = s.actor
+	)
+
+	// Dummy usage to avoid 'declared and not used'
+	// compile errors as we incrementally clean up
+	// the code in this function while converting
+	// commands to use parser state
+	_ = words
+	_ = t
+	_ = cmd
+	_ = msg
+	_ = ok
 
 	switch cmd {
 	case "N", "NE", "E", "SE", "S", "SW", "W", "NW", "U", "D":
-		return Move(t, cmd)
+		msg, ok = Move(t, cmd)
 	case "NORTH", "NORTHEAST", "EAST", "SOUTHEAST", "SOUTH", "SOUTHWEST", "WEST", "NORTHWEST", "UP", "DOWN":
-		return Move(t, cmd)
+		msg, ok = Move(t, cmd)
 	case "#DUMP":
-		return Dump(t, words[1:])
+		msg, ok = Dump(t, words[0:])
 	case "DROP":
-		return Drop(t, words[1:])
+		msg, ok = Drop(t, words[0:])
 	case "EXAMINE", "EXAM":
-		return Examine(t, words[1:])
+		msg, ok = Examine(t, words[0:])
 	case "GET":
-		return Get(t, words[1:])
+		msg, ok = Get(t, words[0:])
 	case "INVENTORY", "INV":
-		return Inventory(t)
+		msg, ok = Inventory(t)
 	case "LOOK", "L":
-		return Look(t)
+		msg, ok = Look(t)
 	case "PUT":
-		return Put(t, words[1:])
+		msg, ok = Put(t, words[0:])
 	case "READ":
-		return Read(t, words[1:])
+		msg, ok = Read(t, words[0:])
 	case "TAKE":
-		return Take(t, words[1:])
+		msg, ok = Take(t, words[0:])
 	case "VERSION":
-		return Version(t)
+		msg, ok = Version(t)
 	default:
-		return "Eh?", false
+		msg, ok = "Eh?", false
+	}
+
+	if msg != "" {
+		s.msg.actor.WriteString(msg)
+		s.ok = ok
 	}
 }
