a0b31c0ea1c37ccdef608525176438e96cca7730
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2016-03-11 18:25:03 +0000

    stats: Don't use defer where there is only one exit path
    Using defer is very useful when there are multiple exit paths from a function
    or method. However if there is only one exit path it is unnecessary to defer
    and using defer is/was slower.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- stats/players.go
+++ stats/players.go
@@ -22,15 +22,13 @@ var players = struct {
 // Add adds the specified player to the list of players.
 func Add(player has.Thing) {
 	players.Lock()
-	defer players.Unlock()
-
 	players.list = append(players.list, player)
+	players.Unlock()
 }
 
 // Remove removes the specified player from the list of players.
 func Remove(player has.Thing) {
 	players.Lock()
-	defer players.Unlock()
 
 	for i, p := range players.list {
 		if p == player {
@@ -47,13 +45,14 @@ func Remove(player has.Thing) {
 		log.Printf("Last one out reclaims the player list: %d slots reclaimed", cap(players.list)-10)
 		players.list = make([]has.Thing, 0, 10)
 	}
+
+	players.Unlock()
 }
 
 // List returns the names of all players in the player list. The omit parameter
 // may be used to specify a player that should be omitted from the list.
 func List(omit has.Thing) []string {
 	players.Lock()
-	defer players.Unlock()
 
 	list := make([]string, 0, len(players.list))
 
@@ -64,13 +63,14 @@ func List(omit has.Thing) []string {
 		list = append(list, attr.FindName(player).Name("Someone"))
 	}
 
+	players.Unlock()
 	return list
 }
 
 // Len returns the length of the player list.
-func Len() int {
+func Len() (l int) {
 	players.Lock()
-	defer players.Unlock()
-
-	return len(players.list)
+	l = len(players.list)
+	players.Unlock()
+	return
 }
