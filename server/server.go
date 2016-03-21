7247354bafe3f267c5b6adfba3369547e7721bc9
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2016-03-21 20:18:44 +0000

    comms,server: Switch to new zones package + drop setup
    This change finally lets us drop the attr/setup.go by switching to the new
    zones package. It also lets us move the initialisation code from listener to
    server which is much more logical.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- server/server.go
+++ server/server.go
@@ -9,6 +9,7 @@ import (
 	"code.wolfmud.org/WolfMUD.git/comms"
 	"code.wolfmud.org/WolfMUD.git/config"
 	"code.wolfmud.org/WolfMUD.git/stats"
+	"code.wolfmud.org/WolfMUD.git/zones"
 
 	"log"
 	"math/rand"
@@ -24,5 +25,6 @@ func main() {
 
 	config.Load()
 	stats.Start()
+	zones.Load()
 	comms.Listen(config.Server.Host, config.Server.Port)
 }
