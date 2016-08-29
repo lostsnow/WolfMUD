5212a451df5a2808b1ed32caff539d7d4ee27384
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2016-08-29 19:31:31 +0100

    frontend: Document and cleanup login
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- frontend/login.go
+++ frontend/login.go
@@ -19,11 +19,15 @@ import (
 	"path/filepath"
 )
 
+// accountDisplay asks for the player's account ID so that they can log into
+// the system.
 func (f *frontend) accountDisplay() {
 	f.buf.WriteString("Please enter your account ID or just press enter to create a new account:")
 	f.nextFunc = f.accountProcess
 }
 
+// accountProcess takes the current input and stores it in the current state as
+// an account ID hash. At this point it is not validated yet just stored.
 func (f *frontend) accountProcess() {
 	if len(f.input) == 0 {
 		f.buf.WriteString("Account creation unavailable.\n")
@@ -36,19 +40,30 @@ func (f *frontend) accountProcess() {
 	f.passwordDisplay()
 }
 
+// passwordDisplay asks for the player's password for their account ID.
 func (f *frontend) passwordDisplay() {
 	f.buf.WriteString("Please enter the password for your account or just press enter to abort:")
 	f.nextFunc = f.passwordProcess
 }
 
+// passwordProcess takes the current input and treats is as the player's
+// password for logging into the system. If no password is entered processing
+// goes back to asking for the players account ID. If the account ID is valid
+// and the password is correct we load the player data and move on to
+// displaying the main menu. If either the account ID or password is invalid we
+// go back to asking for an account ID.
 func (f *frontend) passwordProcess() {
+
+	// If no password given go back and ask for an account ID.
 	if len(f.input) == 0 {
 		f.buf.WriteString("No Password given.\n")
 		f.accountDisplay()
 		return
 	}
 
-	// Can we get the account file?
+	// Can we open the account file? The filename is the MD5 hash of the account
+	// ID. That way the filename is of a know format [0-9a-f]{32}\.wrj and we
+	// don't have to trust user input for filenames hitting the filesystem.
 	p := filepath.Join(config.Server.DataDir, "players", f.account+".wrj")
 	wrj, err := os.Open(p)
 	if err != nil {
@@ -58,6 +73,7 @@ func (f *frontend) passwordProcess() {
 		return
 	}
 
+	// Read the account file as a recordjar
 	jar := recordjar.Read(wrj, "description")
 	if err := wrj.Close(); err != nil {
 		log.Printf("Error closing account: %s", err)
@@ -66,13 +82,31 @@ func (f *frontend) passwordProcess() {
 		return
 	}
 
-	data := jar[0]
-	hash := sha512.Sum512(append(data["SALT"], f.input...))
-	if (base64.URLEncoding.EncodeToString(hash[:])) != string(data["PASSWORD"]) {
+	// The recordjar should have at least two records: account header and player.
+	// If not something is wrong with the data.
+	if len(jar) < 2 {
+		log.Printf("Account file corrupted: %s.wrj", f.account)
+		f.buf.WriteString("Sorry, there is a problem with your account, please contact the admins.\n")
+		f.accountDisplay()
+		return
+	}
+
+	var (
+		record   = jar[0]
+		salt     = recordjar.Decode.Bytes(record["SALT"])
+		password = recordjar.Decode.String(record["PASSWORD"])
+		hash     = sha512.Sum512(append(salt, f.input...))
+	)
+
+	// Check password is valid
+	if (base64.URLEncoding.EncodeToString(hash[:])) != password {
+		log.Printf("Password invalid for: %s.wrj", f.account)
 		f.buf.WriteString("Acount ID or password is incorrect.\n")
 		f.accountDisplay()
 		return
 	}
+
+	// Drop header record from jar
 	jar = jar[1:]
 
 	// Check if account already in use to prevent multiple logins
@@ -88,8 +122,9 @@ func (f *frontend) passwordProcess() {
 	accounts.Unlock()
 
 	// Assemble player
+	record = jar[0]
 	f.player = attr.NewThing()
-	f.player.(*attr.Thing).Unmarshal(0, jar[0])
+	f.player.(*attr.Thing).Unmarshal(1, record)
 	f.player.Add(attr.NewLocate(nil))
 	f.player.Add(attr.NewPlayer(f.output))
 
