c64acc02c050c0dd14588d9a7df262a5a7064efd
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2016-11-23 20:07:47 +0000

    message: Split up buffer, buffers & msg + tests
    This change splits up buffer, buffers and msg into their own source
    files. It also splits the tests into buffer and buffers tests. This
    should aid future manageability. Need a better name for msg.go but it
    will do for now.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- message/buffer.go
+++ message/buffer.go
@@ -6,8 +6,6 @@
 package message
 
 import (
-	"code.wolfmud.org/WolfMUD.git/has"
-
 	"io"
 )
 
@@ -43,26 +41,6 @@ func NewBuffer() (b *buffer) {
 	b = &buffer{}
 	b.omitLF = true
 	return
-}
-
-// buffers are a collection of buffer indexed by location.
-type buffers map[has.Inventory]*buffer
-
-// Msg is a collection of buffers for gathering messages to send back as a
-// result of processing a command. Before use a Msg should have Allocate called
-// on it to allocate and setup the buffers internally. After use Deallocate
-// should be called to free up the buffers. The Allocate and Deallocate methods
-// are kept separate so that a Msg can be reused by repeated calls to
-// Allocate/Deallocate.
-//
-// NOTE: Observer is setup as an 'alias' for Observers[s.where] - Observer and
-// Observers[s.where] point to the same buffer. See the Allocate method for
-// more details.
-type Msg struct {
-	Actor       *buffer
-	Participant *buffer
-	Observer    *buffer
-	Observers   buffers
 }
 
 // Send takes a number of strings and writes them into the buffer as a single
@@ -160,114 +138,5 @@ func (b *buffer) Deliver(w io.Writer) {
 	if b.omitLF {
 		b.buf = b.buf[0:0]
 		b.count = 0
-	}
-}
-
-// Send calls buffer.Send for each buffer in the receiver buffers.
-//
-// See also buffer.Send for more details.
-func (b buffers) Send(s ...string) {
-	for _, b := range b {
-		b.Send(s...)
-	}
-}
-
-// Append calls buffer.Append for each buffer in the receiver buffers.
-//
-// See also buffer.Append for more details.
-func (b buffers) Append(s ...string) {
-	for _, b := range b {
-		b.Append(s...)
-	}
-}
-
-// Silent calls buffer.Silent with the passed new flag for each buffer in the
-// receiver buffers. Silent returns two sets of buffers, one for all buffers
-// that were true and one for all buffers that were false. The previous silent
-// state of buffers can be restored by calling Silent with true or false on the
-// returned buffers. For example:
-//
-//	t,f := s.msg.Observers.Silent(true)
-//	:
-//	: // do something
-//	:
-//	t.Silent(true)
-//	f.silent(false)
-//
-// See also buffer.Silent for more details.
-func (b buffers) Silent(new bool) (t buffers, f buffers) {
-	t = make(map[has.Inventory]*buffer)
-	f = make(map[has.Inventory]*buffer)
-	for where, b := range b {
-		if old := b.Silent(new); old {
-			t[where] = b
-		} else {
-			f[where] = b
-		}
-	}
-	return
-}
-
-// Len returns the number of messages for each buffer in buffers as a
-// [has.Inventory]int map.
-func (b buffers) Len() (l map[has.Inventory]int) {
-	l = make(map[has.Inventory]int)
-	for where, b := range b {
-		l[where] = b.count
-	}
-	return
-}
-
-// Filter takes a list of Inventories and returns only matching buffer entries
-// as buffers.
-func (b buffers) Filter(limit ...has.Inventory) (filtered buffers) {
-	filtered = make(map[has.Inventory]*buffer)
-	for _, l := range limit {
-		if _, ok := b[l]; ok {
-			filtered[l] = b[l]
-		}
-	}
-	return
-}
-
-// Allocate sets up the message buffers for the actor, participant and
-// observers. The 'where' passed in should be the current location so that
-// Observer can be linked to the correct Observers element. The locks passed in
-// are used to setup a buffer for observers in each of the locations being
-// locked.
-//
-// The actor's buffer is initially set to half a page (half of 80 columns by 24
-// lines) as it is common to be sending location descriptions back to the
-// actor. Half a page is arbitrary but seems to be reasonable.
-//
-// NOTE: For crowded locations buffers of observers are automatically put in
-// silent mode.
-func (m *Msg) Allocate(where has.Inventory, locks []has.Inventory) {
-	if m.Actor == nil {
-		m.Actor = &buffer{buf: make([]byte, 0, (80*24)/2)}
-		m.Actor.omitLF = true
-		m.Participant = &buffer{}
-		m.Observers = make(map[has.Inventory]*buffer)
-	}
-
-	for _, l := range locks {
-		if _, ok := m.Observers[l]; !ok {
-			m.Observers[l] = &buffer{}
-			m.Observers[l].Silent(l.Crowded())
-		}
-	}
-	m.Observer = m.Observers[where]
-}
-
-// Deallocate releases the references to message buffers for the actor,
-// participant and observers. Specific deallocation can help with garbage
-// collection.
-func (m *Msg) Deallocate() {
-	m.Actor = nil
-	m.Participant = nil
-	m.Observer = nil
-	for where := range m.Observers {
-		m.Observers[where] = nil
-		delete(m.Observers, where)
 	}
 }
--- message/buffer_test.go
+++ message/buffer_test.go
@@ -6,9 +6,6 @@
 package message
 
 import (
-	"code.wolfmud.org/WolfMUD.git/attr"
-	"code.wolfmud.org/WolfMUD.git/has"
-
 	"bytes"
 	"fmt"
 	"strings"
@@ -23,16 +20,6 @@ func collectDelivery(b *buffer) (data []byte) {
 	b.Deliver(w)
 	data = make([]byte, w.Len())
 	w.Read(data)
-	return
-}
-
-// newBuffers is a simple helper to populate buffers with a given number of
-// buffer keyed by different Inventory.
-func newBuffers(c int) (b buffers) {
-	b = buffers{}
-	for x := 0; x < c; x++ {
-		b[attr.NewInventory()] = &buffer{}
-	}
 	return
 }
 
@@ -517,182 +504,6 @@ func TestCombo(t *testing.T) {
 					}
 				}
 			})
-		}
-	}
-}
-
-// TestGroupSend tests sending messages to a group of buffer.
-func TestGroupSend(t *testing.T) {
-
-	// Use from 0 to 9 buffers
-	for c := 0; c < 10; c++ {
-
-		bufs := newBuffers(c)
-		bufs.Send("Hello World!")
-
-		// Check delivery of messages
-		for _, b := range bufs {
-			{
-				have := collectDelivery(b)
-				want := "\nHello World!"
-
-				if !bytes.Equal(have, []byte(want)) {
-					t.Errorf("GroupSend %d buffers have: %+q want: %+q", c, have, want)
-				}
-			}
-		}
-	}
-}
-
-// TestGroupSendAppend tests sending and appending messages to a group of
-// buffer.
-func TestGroupSendAppend(t *testing.T) {
-
-	// Use from 0 to 9 buffers
-	for c := 0; c < 10; c++ {
-
-		bufs := newBuffers(c)
-		bufs.Send("Hello")
-		bufs.Append("World!")
-
-		// Check delivery of messages
-		for _, b := range bufs {
-			{
-				have := collectDelivery(b)
-				want := "\nHello World!"
-
-				if !bytes.Equal(have, []byte(want)) {
-					t.Errorf("GroupSendAppend %d buffers have: %+q want: %+q", c, have, want)
-				}
-			}
-		}
-	}
-}
-
-// TestGroupSilent tests setting silent mode on a group of buffer.
-func TestGroupSilent(t *testing.T) {
-
-	// Use from 0 to 9 buffers
-	for c := 0; c < 10; c++ {
-
-		bufs := newBuffers(c)
-		bufs.Silent(true)
-		bufs.Send("Hello World!")
-
-		// Check delivery of messages
-		for _, b := range bufs {
-			{
-				have := collectDelivery(b)
-				want := ""
-
-				if !bytes.Equal(have, []byte(want)) {
-					t.Errorf("GroupSilent buffer have: %+q want: %+q", have, want)
-				}
-			}
-		}
-	}
-}
-
-// TestGroupSilentReturn tests the state returned by Silent on a group of
-// buffer.
-func TestGroupSilentReturn(t *testing.T) {
-
-	// Use from 0 to 9 buffers
-	for c := uint(0); c < 10; c++ {
-
-		// Setup silent pattern, 1 bit per buffer, going through all patterns
-		for s := uint(0); s <= (1<<c)-1; s++ {
-
-			// Maps for which inventories we want to be true (wt) and false (wf)
-			wt := make(map[has.Inventory]struct{})
-			wf := make(map[has.Inventory]struct{})
-
-			// Setup buffers
-			bufs := buffers{}
-			for x := uint(0); x < c; x++ {
-				b := &buffer{}
-				where := attr.NewInventory()
-				bufs[where] = b
-
-				// Set silent according to bit set in s pattern and store inventory in
-				// matching wanted true/false lists
-				if s&(1<<x) != 0 {
-					b.Silent(true)
-					wt[where] = struct{}{}
-				} else {
-					wf[where] = struct{}{}
-				}
-			}
-
-			// Set all buffers silent to retrieve previous state as have true (ht)
-			// and have false (hf) lists
-			ht, hf := bufs.Silent(true)
-
-			// Check we have expected buffer in true lists
-			for where := range wt {
-				if _, ok := ht[where]; !ok {
-					t.Errorf("GroupSilentReturn buffer not in true list")
-				}
-			}
-
-			// Check we have expected buffer in false lists
-			for where := range wf {
-				if _, ok := hf[where]; !ok {
-					t.Errorf("GroupSilentReturn buffer not in false list")
-				}
-			}
-		}
-	}
-}
-
-// TestGroupLen tests the message count returned by a group of buffer.
-func TestGroupLen(t *testing.T) {
-	bufs := newBuffers(10)
-	want := make(map[has.Inventory]int)
-
-	// Send a different, incrementing number of messages to each buffer. Record
-	// number sent to each buffer in want list.
-	x := 0
-	for where, buf := range bufs {
-		want[where] = x
-		for y := 0; y < x; y++ {
-			buf.Send("Hello World!")
-		}
-		x++
-	}
-
-	// Get number of messages in each buffer
-	have := bufs.Len()
-
-	// Make sure we have expected number of messages in each buffer
-	for where, l := range have {
-		if want[where] != l {
-			t.Errorf("GroupLen wrong message count have: %d want: %d", l, want[where])
-		}
-	}
-}
-
-// TestGroupFilter tests that the correct buffer are returned by Filter.
-func TestGroupFilter(t *testing.T) {
-	bufs := newBuffers(10)
-	want := []has.Inventory{}
-
-	for where := range bufs {
-
-		// Increase want list one buffer at a time and gradually filter buffers
-		want = append(want, where)
-		have := bufs.Filter(want...)
-
-		// Check we have the expected number of buffer returned by Filter
-		if lh, lw := len(have), len(want); lh != lw {
-			t.Errorf("GroupFilter wrong number of buffers have: %d want: %d", lh, lw)
-		}
-
-		// Check buffers returned are the ones we wanted
-		for _, where := range want {
-			if _, ok := have[where]; !ok {
-				t.Errorf("GroupFilter wrong buffer found")
-			}
 		}
 	}
 }
