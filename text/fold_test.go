dc5a5df479d65f618d0317f3a794b7bb8f376b10
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2016-12-30 22:51:33 +0000

    text: change "\033[39m" to "\033[0m" for tests
    Should now be using "\033[0m" text.Reset instead of "\033[39m"
    text.Default.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- text/fold_test.go
+++ text/fold_test.go
@@ -125,18 +125,18 @@ var testCases = []struct {
 	{"Unicode \u0061\u0300", 9, "Unicode \u0061\u0300"},
 
 	// Control sequences should be zero width
-	{"\033[31mWolfMUD\033[39m", 7, "\033[31mWolfMUD\033[39m"},
-	{"\n\033[31mWolfMUD\033[39m\n", 7, "\r\n\033[31mWolfMUD\033[39m\r\n"},
-	{"\033[31mWolfMUD WolfMUD\033[39m", 20, "\033[31mWolfMUD WolfMUD\033[39m"},
-	{"\n\033[31mWolfMUD WolfMUD\033[39m\n", 20, "\r\n\033[31mWolfMUD WolfMUD\033[39m\r\n"},
-	{"\n\033[31mWolfMUD WolfMUD\033[39m\n", 20, "\r\n\033[31mWolfMUD WolfMUD\033[39m\r\n"},
-	{"\033[31mWolfMUD \033[32mWolfMUD\033[39m", 7, "\033[31mWolfMUD\r\n\033[32mWolfMUD\033[39m"},
-	{"\033[31mWolfMUD\033[32mWolfMUD\033[39m", 7, "\033[31mWolfMUD\033[32mWolfMUD\033[39m"},
-	{"\033[31mWolfMUD\n\033[32mWolfMUD\033[39m", 7, "\033[31mWolfMUD\r\n\033[32mWolfMUD\033[39m"},
-	{"\033[31mWolfMUD\n \033[32mWolfMUD\033[39m", 7, "\033[31mWolfMUD\r\n \033[32mWolfMUD\033[39m"},
-	{"\033[31mWolfMUD\n\033[32m WolfMUD\033[39m", 7, "\033[31mWolfMUD\r\n\033[32m WolfMUD\033[39m"},
-	{"\033[31mWolfMUD\n  \033[32mWolfMUD\033[39m", 7, "\033[31mWolfMUD\r\n  \033[32mWolfMUD\033[39m"},
-	{"\033[31mWolfMUD\n\033[32m  WolfMUD\033[39m", 7, "\033[31mWolfMUD\r\n\033[32m  WolfMUD\033[39m"},
+	{"\033[31mWolfMUD\033[0m", 7, "\033[31mWolfMUD\033[0m"},
+	{"\n\033[31mWolfMUD\033[0m\n", 7, "\r\n\033[31mWolfMUD\033[0m\r\n"},
+	{"\033[31mWolfMUD WolfMUD\033[0m", 20, "\033[31mWolfMUD WolfMUD\033[0m"},
+	{"\n\033[31mWolfMUD WolfMUD\033[0m\n", 20, "\r\n\033[31mWolfMUD WolfMUD\033[0m\r\n"},
+	{"\n\033[31mWolfMUD WolfMUD\033[0m\n", 20, "\r\n\033[31mWolfMUD WolfMUD\033[0m\r\n"},
+	{"\033[31mWolfMUD \033[32mWolfMUD\033[0m", 7, "\033[31mWolfMUD\r\n\033[32mWolfMUD\033[0m"},
+	{"\033[31mWolfMUD\033[32mWolfMUD\033[0m", 7, "\033[31mWolfMUD\033[32mWolfMUD\033[0m"},
+	{"\033[31mWolfMUD\n\033[32mWolfMUD\033[0m", 7, "\033[31mWolfMUD\r\n\033[32mWolfMUD\033[0m"},
+	{"\033[31mWolfMUD\n \033[32mWolfMUD\033[0m", 7, "\033[31mWolfMUD\r\n \033[32mWolfMUD\033[0m"},
+	{"\033[31mWolfMUD\n\033[32m WolfMUD\033[0m", 7, "\033[31mWolfMUD\r\n\033[32m WolfMUD\033[0m"},
+	{"\033[31mWolfMUD\n  \033[32mWolfMUD\033[0m", 7, "\033[31mWolfMUD\r\n  \033[32mWolfMUD\033[0m"},
+	{"\033[31mWolfMUD\n\033[32m  WolfMUD\033[0m", 7, "\033[31mWolfMUD\r\n\033[32m  WolfMUD\033[0m"},
 }
 
 func TestFold(t *testing.T) {
