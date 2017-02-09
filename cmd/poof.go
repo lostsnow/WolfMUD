80fd314de847c5a715bdb7f6091b4f8f8595ea11
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2017-02-09 23:12:47 +0000

    cmd: New scripting command $POOF
    The $POOF command is used when placing a player in the world to announce
    their arrival. Observers will see a message like:
    
      There is a cloud of smoke from which Diddymus emerges coughing and
      spluttering.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

---
+++ cmd/poof.go
@@ -0,0 +1,24 @@
+// Copyright 2017 Andrew 'Diddymus' Rolfe. All rights reserved.
+//
+// Use of this source code is governed by the license in the LICENSE file
+// included with the source code.
+
+package cmd
+
+import (
+	"code.wolfmud.org/WolfMUD.git/attr"
+)
+
+// Syntax: $POOF
+func init() {
+	AddHandler(Poof, "$POOF")
+}
+
+func Poof(s *state) {
+
+	name := attr.FindName(s.actor).Name("Someone")
+
+	s.msg.Observer.SendGood("There is a cloud of smoke from which ", name, " emerges coughing and spluttering.")
+
+	s.scriptActor("LOOK")
+}
