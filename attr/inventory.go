26b3b6aa2741c7fad0eb3ed2b49d10862d14ba8b
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2017-03-26 18:07:21 +0100

    attr: Inventory.Move check if a Thing respawnable when removed
    When a Thing is removed from an Inventory through either the Move or
    Remove methods a check is now made to see if the Thing should be
    scheduled to respawn. If it should respawn a non-respawnable copy of the
    Thing is returned and the original is scheduled. In this way respawning
    items are handled automatically and various commands such as GET and
    TAKE do not have to implement their own functionality.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- attr/inventory.go
+++ attr/inventory.go
@@ -181,6 +181,15 @@ func (i *Inventory) Move(t has.Thing, to has.Inventory) has.Thing {
 				i.split--
 			}
 
+			// If not a player check if removing a Thing triggers a re-spawning.
+			// Players don't respawn but they do move from location to location a lot
+			// which would cause needless calls to Spawn.
+			if !p {
+				if s := FindReset(t).Spawn(); s != nil {
+					t = s
+				}
+			}
+
 			found = true
 		}
 	}
