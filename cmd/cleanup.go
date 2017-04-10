ce7d8c7927e373b0b935d1c766517bc6e97e15ac
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2017-04-10 19:17:30 +0100

    has,attr,cmd,docs: New OnCleanup attribute
    OnClean is a simple, single string attribute that can be used to specify
    a message to be displayed when an item is cleaned up.
    
    The OnCleanup attribute and interface just provide the basic message.
    
    The $cleanup command has been updated to control when the message is
    displayed. If the clean up occurs at a location where it will be seen
    then the message will be displayed. If no message has been provided a
    default one will be used. If there should be no notification of the
    clean up then an OnCleanup attribute with no message will suppress the
    default message.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- cmd/cleanup.go
+++ cmd/cleanup.go
@@ -7,7 +7,6 @@ package cmd
 
 import (
 	"code.wolfmud.org/WolfMUD.git/attr"
-	"code.wolfmud.org/WolfMUD.git/text"
 )
 
 // Syntax: $CLEANUP
@@ -17,8 +16,38 @@ func init() {
 
 func Cleanup(s *state) {
 
-	name := attr.FindName(s.actor).Name("something")
-	s.msg.Observer.SendInfo(text.TitleFirst(name), " fades away and is gone.")
+	// Find Inventory where clean up is going to take place and make sure we are
+	// locking it
+	to := attr.FindLocate(s.actor).Origin()
+	if !s.CanLock(to) {
+		s.AddLock(to)
+		return
+	}
+
+	oc := attr.FindOnCleanup(s.actor)
+	msg := oc.CleanupText()
+
+	l := s.where
+	p := l.Parent()
+	e := attr.FindExits(p)
+
+	// Clean up will not be seen if it does not happen in a location. It also
+	// will not be seen if we have specifically have an empty message. So just
+	// junk Thing.
+	if !e.Found() || (oc.Found() && msg == "") {
+		alias := s.actor.UID()
+		s.scriptNone("junk", alias)
+		s.ok = true
+		return
+	}
+
+	// Clean up will be seen so add default message if we don't have one
+	if !oc.Found() {
+		name := attr.FindName(s.actor).Name("something")
+		msg = "You are sure you noticed " + name + " here, but you can't see it now."
+	}
+
+	s.msg.Observers[l].SendInfo(msg)
 
 	alias := s.actor.UID()
 	s.scriptNone("junk", alias)
