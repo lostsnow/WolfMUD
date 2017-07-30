f02a858563649480217aa9d9b1a9ac8fa315e05b
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2017-07-30 15:21:35 +0100

    cmd: Clarify comment in JUNK command
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- cmd/junk.go
+++ cmd/junk.go
@@ -86,7 +86,7 @@ func Junk(s *state) {
 
 	// Remove Thing from Inventory where it is. A respawn will be triggered if
 	// Thing is spawnable and we will get the copy. Otherwise we get the original
-	// back.
+	// back. If we get nil returned the Thing could not be removed for junking.
 	if what = where.Remove(what); what == nil {
 		s.msg.Actor.SendBad("For some reason you cannot junk ", name, ".")
 		return
