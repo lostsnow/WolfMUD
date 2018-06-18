da5c46b7ba91dad2ed5e85b7040709443fbcae06
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2018-06-18 18:25:12 +0100

    log: New per connection logging
    This commit adds a new log package and a per connection logger helper.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

---
+++ log/connection.go
@@ -0,0 +1,25 @@
+// Copyright 2018 Andrew 'Diddymus' Rolfe. All rights reserved.
+//
+// Use of this source code is governed by the license in the LICENSE file
+// included with the source code.
+
+package log
+
+import (
+	"fmt"
+	"log"
+)
+
+// Conn represents a per connection logging function.
+type Conn func(fmt string, v ...interface{})
+
+// NewConn returns a per connection logging function. The function prefixes the
+// messages with "[n]" where n is the sequence number of the connection. The
+// function is called in exactly the same way as log.Printf in the standard
+// library. The output will be written to the standard logger.
+func NewConn(seq uint64) Conn {
+	s := fmt.Sprintf("[%d] ", seq)
+	return func(fmt string, args ...interface{}) {
+		log.Printf(s+fmt, args...)
+	}
+}
