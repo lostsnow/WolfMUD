lostsnow/WolfMUD

commit 50b52091f70ee42abefc2e3493192c1e3a41c8bd

attr: Description.Marshal use encode.Bytes to preserve '\n'
When marshaling descriptions use encode.Bytes so that leading and
trailing line feeds are preserved for formatting in the free text
section.

Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

attr/description.go
@@ -68,7 +68,7 @@ func (*Description) Unmarshal(data []byte) has.Attribute {
 
 // Marshal returns a tag and []byte that represents the receiver.
 func (d *Description) Marshal() (tag string, data []byte) {
-	return "description", encode.String(d.description)
+	return "description", encode.Bytes(d.description)
 }
 
 func (d *Description) Dump() []string {

commit 7107da2761aff00032e01afdd1650f89820393d4

attr: Fix compile failure, Description needs string→[]byte conversion
Commit 50b52091f70ee42 introduced a compile failure. When switching from
encode.Strgin to encode.Bytes a conversion from string to []byte is also
required.

Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

attr/description.go
@@ -68,7 +68,7 @@ func (*Description) Unmarshal(data []byte) has.Attribute {
 
 // Marshal returns a tag and []byte that represents the receiver.
 func (d *Description) Marshal() (tag string, data []byte) {
-	return "description", encode.Bytes(d.description)
+	return "description", encode.Bytes([]byte(d.description))
 }
 
 func (d *Description) Dump() []string {