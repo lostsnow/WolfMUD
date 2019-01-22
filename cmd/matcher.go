c2f16d420dbe63aa29fb383b52fb8e06d0b256da
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2019-01-22 20:42:14 +0000

    cmd: clean up matcher
    This commit is a clean up of the matcher. Documentation for MatchLimit
    has been rewritten. The special conditional matching code has been split
    out into a new specialQualifier function. Some variables have been
    renamed for clarity.
    
    Note that the comments for MatchLimit describe an updated PUT command
    that has not actually been updated yet!
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- cmd/matcher.go
+++ cmd/matcher.go
@@ -13,49 +13,88 @@ import (
 	"code.wolfmud.org/WolfMUD.git/has"
 )
 
-// Match is shorthand for MatchLimit with a limit of 1.
-func Match(wordList []string, inv ...[]has.Thing) (matches []has.Thing, unknowns, words []string) {
-	matches, unknowns, words = MatchLimit(wordList, 1, inv...)
-	return
-}
-
-// MatchAll is shorthand for MatchLimit with a limit of -1.
-func MatchAll(wordList []string, inv ...[]has.Thing) (matches []has.Thing, unknowns, words []string) {
-	matches, unknowns, words = MatchLimit(wordList, -1, inv...)
-	return
-}
-
-// MatchLimit tries to find Things in the given inventories by matching
-// aliases and qualifiers in the given word list. If a limit of -1 is specified
-// MatchLimit will attempt to consume all of the words in the word list. If a
-// specific limit X is given only enough words will be consumed to identify X
-// sets of matches. Each set of matches will contain the Things the consumed
-// words matched. It should also be noted that words are consumed from the list
-// right to left. So given a word list of "apple ball cat", cat will be
-// searched for first, then ball then apple.
+// MatchLimit tries to find Things in the given Inventory by matching aliases
+// and qualifiers in the given word list. The limit is the number of groups of
+// words to match. A group of words consisting of an alias plus zero or more
+// qualifiers. A limit of -1 will consume all words in the list.
 //
-// An example, given "large ball small ball" as the word list. A limit of 1
-// could consume two words if "small ball" was found to match some Things. The
-// returned matches would include all Things that matched "small ball".
+// Assume the following items, aliases and qualifiers in square brackets and
+// qualifiers have a leading '+' symbol:
 //
-// Given the same list with a limit of -1 all words would be consumed and the
-// returned matches would contain all the Things matching "large ball" or
-// "small ball".
+//  a small green ball  [+SMALL +GREEN BALL]
+//  a small red ball    [+SMALL +RED BALL]
+//  a large green ball  [+LARGE +GREEN BALL]
+//  a large red ball    [+LARGE +RED BALL]
 //
-// If the word list in the example was replaced with "ball" the matches
-// returned would include all large and small balls found even though only one
-// word in the word would be consumed.
+// All matching processes the word list from right to left. This is because a
+// group of words identifying one or more items always ends with an alias and
+// can be preceded by one or more qualifiers. By default only the first item
+// matching a word group will be returned.
 //
-// Words not consumed are returned so that they may be passed to consecutive
-// searches for further processing.
+// With a word list of 'RED BALL GREEN BALL' a limit of 1 would consume two
+// words 'GREEN BALL' and return one Thing matched: a small green ball. In this
+// case the alias 'BALL' would match, followed by the qualifier 'GREEN'. As
+// 'BALL' preceding 'GREEN' is not a qualifier matching fails and we have
+// 'GREEN BALL' as the first matching group of words.
 //
-// For example in the command "PUT <items...> <container>" you would call
-// Match to find the container, then MatchAll to find the remaining items. If
-// you wanted to limit the PUT command to "PUT <item> <container>" you would
-// call Match to find the container, then Match to find the item. If the word
-// list is not zero at the end then too many items have been specified.
-// Likewise if the list of matches is not one then more than one container/item
-// has been found.
+// With a word list of 'RED BALL GREEN BALL' a limit of 2 (or -1) would consume
+// all of the words and return two Thing matched: a small green ball, a small
+// red ball. Note that the limit of 2 if the number of word groups, in this
+// case 'RED BALL' and 'GREEN BALL' not the number of items to return.
+//
+// Within a matching group of words the first qualifier may be special. The
+// special qualifiers are:
+//
+//  ALL - include all items matching the current group
+//  n - include up to n items matching the current group
+//  nth - include only the nth item (postfix may be ST, ND, RD or TH)
+//  n-N - include only matching items n to N for the current group
+//
+// With a word list of 'ALL GREEN BALL' all items matching the alias of 'BALL'
+// with a qualifier of 'GREEN' will be returned. In this case: a small green
+// ball, a large green ball.
+//
+// With a word list of '2 BALL' the first 2 items matching the alias of 'BALL'
+// will be returned. In this case: a small green ball, a small red ball.
+//
+// With a word list of '2ND RED BALL' only the second item matching the alias
+// of 'BALL' with a qualifier of 'RED' will be returned. In this case: a large
+// red ball.
+//
+// With a word list of '2-3 BALL' items 2 through to 3 matching the alias of
+// 'BALL' would be returned. In this case: a small red ball, a large green
+// ball.
+//
+// Duplicate matching items will be removed from the results. With a word list
+// of 'ALL GREEN BALL 2-3 BALL' then 'ALL GREEN BALL' would match: a small green
+// ball, a large green ball and '2-3 BALL' would match: a small red ball, a
+// large green ball. The results returned would be: a small green ball, a small
+// red ball, a large green ball.
+//
+// When matching an alias and zero or more qualifiers the first non-match will
+// end the group. Given the word list 'RED BALL GREEN BALL', after identifying
+// the alias 'BALL' and qualifier 'GREEN' the word 'BALL' fails to match as a
+// qualifier. Therefore the first matching word group is 'GREEN BALL'. Matching
+// then starts again to look for an alias for 'BALL' with a qualifier of 'RED'.
+//
+// If a word fails to match as an alias, or fails to match as a qualifier after
+// an alias match, it is added to the returned unknowns list. Consecutive
+// unknown words are grouped together. Given the word list 'GREEN BALL BLUE
+// FROG RED BALL', after identifying 'RED BALL' the word 'FROG' is not matched
+// and added to the unknowns. Then 'BLUE' is also unmatched and the unknowns
+// become 'BLUE FROG' because the unknowns are consecutive. Given the word list
+// 'TREE GREEN BALL FROG RED BALL', then 'FROG' and 'TREE' would be unmatched.
+// As they are not consecutive - 'GREEN BALL' would match between 'TREE' and
+// 'FROG' two unknowns would be returned 'TREE' and 'FROG'.
+//
+// There may be some instances where different matches need to be carried out
+// against different Inventory. For example the PUT command. The PUT command
+// takes the form: PUT <item ...> <container>. The container may be carried or
+// at the current location, while items to put in it should be carried. In this
+// instance the container can be checked by calling Match, the items can then
+// be found by calling MatchAll with the word list returned by the initial
+// Match to complete the matching. In this way different Inventory can be
+// passed to Match and MatchAll.
 func MatchLimit(wordList []string, limit int, inv ...[]has.Thing) (matches []has.Thing, unknowns, words []string) {
 
 	// Get a working list of the words in reverse order
@@ -65,24 +104,23 @@ func MatchLimit(wordList []string, limit int, inv ...[]has.Thing) (matches []has
 	}
 
 	// unknownSet is the list of unknown matches, consecutive unknown matches are
-	// grouped together. thingMatched is a flag tracking whether the last run of
+	// grouped together. lastMatchGood is a flag tracking whether the last run of
 	// words matched at least one known Thing or not.
 	unknownSet := [][]string{}
-	thingMatched := false
+	lastMatchGood := false
+
 	for len(words) > 0 && (limit == -1 || len(matches) < limit) {
-		maxMatch := 0
 		found := []has.Thing{}
-		minLimit, maxLimit := 0, 1
+		minLimit, maxLimit, maxWordsMatched := 0, 1, 0
 
 		for _, i := range inv {
 			for _, t := range i {
+				wordsMatched := 0
 
 				a := attr.FindAlias(t) // Ignore thing if no aliases
 				if !a.Found() {
 					continue
 				}
-
-				numMatch := 0
 
 				// Try and match alias then as many qualifiers as possible
 			wordLoop:
@@ -98,53 +136,21 @@ func MatchLimit(wordList []string, limit int, inv ...[]has.Thing) (matches []has
 
 					if (x == 0 && !a.HasAlias(word)) || (x > 0 && !a.HasAlias("+"+word)) {
 						if x != 0 {
-
-							// Special qualifier for all matches?
-							if word == "ALL" {
-								maxLimit = 0
-								numMatch++
-							}
-							// Could word be a special numeric qualifier?
-							if split := lastLeadingDigit(word); split != -1 {
-								split++
-								if n, err := strconv.Atoi(word[:split]); err == nil {
-									post := word[split:]
-									// Just a number is a limit from 0-n items
-									if post == "" {
-										maxLimit = n
-										numMatch++
-									}
-									// Number with postfix is a specific instance such as 1st, 2nd
-									if post == "ST" || post == "ND" || post == "RD" || post == "TH" {
-										minLimit, maxLimit = n, n
-										numMatch++
-									}
-									// Number followed by a hyphen and a number (n-N) is a range
-									if strings.HasPrefix(post, "-") {
-										if N, err := strconv.Atoi(post[1:]); err == nil {
-											if N > n {
-												minLimit, maxLimit = n, N
-											} else {
-												minLimit, maxLimit = N, n
-											}
-											numMatch++
-										}
-									}
-								}
-							}
-
+							match := 0
+							minLimit, maxLimit, match = specialQualifier(word)
+							wordsMatched += match
 						}
 						break
 					}
 
-					numMatch++
+					wordsMatched++
 				}
 
-				if numMatch != 0 && numMatch >= maxMatch {
-					if numMatch > maxMatch {
+				if wordsMatched != 0 && wordsMatched >= maxWordsMatched {
+					if wordsMatched > maxWordsMatched {
 						found = found[:0]
 					}
-					maxMatch = numMatch
+					maxWordsMatched = wordsMatched
 					found = append([]has.Thing{t}, found...)
 				}
 			}
@@ -152,17 +158,17 @@ func MatchLimit(wordList []string, limit int, inv ...[]has.Thing) (matches []has
 
 		// If matching fails append word to current unknown word group, starting a
 		// new group if previous match was good.
-		if maxMatch == 0 {
-			if thingMatched == true || len(unknownSet) == 0 {
+		if maxWordsMatched == 0 {
+			if lastMatchGood == true || len(unknownSet) == 0 {
 				unknownSet = append([][]string{[]string{}}, unknownSet...)
 			}
 			unknownSet[0] = append([]string{words[0]}, unknownSet[0]...)
-			thingMatched = false
+			lastMatchGood = false
 			words = words[1:]
 			continue
 		}
 
-		thingMatched = true
+		lastMatchGood = true
 
 		// Append any new good matches ignoring anything already in the list of
 		// matches.
@@ -180,7 +186,7 @@ func MatchLimit(wordList []string, limit int, inv ...[]has.Thing) (matches []has
 			}
 			matches = append([]has.Thing{f}, matches...)
 		}
-		words = words[maxMatch:]
+		words = words[maxWordsMatched:]
 
 	}
 
@@ -189,8 +195,91 @@ func MatchLimit(wordList []string, limit int, inv ...[]has.Thing) (matches []has
 		unknowns = append(unknowns, strings.Join(unknown, " "))
 	}
 
-	// Return words in the correct order
+	// Put remaining words into the correct order
 	words = wordList[:len(words)]
+
+	return
+}
+
+// Match is shorthand for MatchLimit with a limit of 1.
+func Match(wordList []string, inv ...[]has.Thing) (matches []has.Thing, unknowns, words []string) {
+	matches, unknowns, words = MatchLimit(wordList, 1, inv...)
+	return
+}
+
+// MatchAll is shorthand for MatchLimit with a limit of -1.
+func MatchAll(wordList []string, inv ...[]has.Thing) (matches []has.Thing, unknowns, words []string) {
+	matches, unknowns, words = MatchLimit(wordList, -1, inv...)
+	return
+}
+
+// specialQualifier takes a word and tries to process it as a special
+// qualifier. Special qualifiers can be of the form:
+//
+//   ALL - all items
+//   n - limit matches to 0-n
+//   nP - specific instance 1st, 2nd, rd, 4th, etc
+//   n-N - a range of items from n to N
+//
+// specialQualifier will return the new minimum and maximum limits. It will
+// also return match=0 if there is no match or match=1 if there is a match.
+func specialQualifier(word string) (minLimit, maxLimit, match int) {
+
+	// Set default for limit 0-1 and no matches
+	minLimit, maxLimit, match = 0, 1, 0
+
+	// Special qualifier for all matches?
+	if word == "ALL" {
+		minLimit, maxLimit, match = 0, 0, 1
+		return
+	}
+
+	// If qualifier has no leading digits just return, not special
+	split := lastLeadingDigit(word)
+	if split == -1 {
+		return
+	}
+
+	// Move to start of postfix after initial int
+	split++
+
+	// If digits cannot be parsed as an int just return, not special
+	n, err := strconv.Atoi(word[:split])
+	if err != nil {
+		return
+	}
+
+	// Get postfix for a specific instance or range
+	post := word[split:]
+
+	// Just a number is a limit from 0-n items
+	if post == "" {
+		minLimit, maxLimit, match = 0, n, 1
+		return
+	}
+
+	// Number with postfix is a specific instance such as 1st, 2nd
+	if post == "ST" || post == "ND" || post == "RD" || post == "TH" {
+		minLimit, maxLimit, match = n, n, 1
+		return
+	}
+
+	// Number followed by a hyphen and a number (n-N) is a range
+	if strings.HasPrefix(post, "-") {
+		N, err := strconv.Atoi(post[1:])
+
+		// Return if digits after hyphen cannot be parsed as an int
+		if err != nil {
+			return
+		}
+
+		if N > n {
+			minLimit, maxLimit, match = n, N, 1
+		} else {
+			minLimit, maxLimit, match = N, n, 1
+		}
+		return
+	}
 
 	return
 }
