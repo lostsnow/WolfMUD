eec992be8f6e068d94e30b45580897135e463981
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2019-04-21 18:11:01 +0100

    cmd: Reimplement DROP command using new matcher
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- cmd/drop.go
+++ cmd/drop.go
@@ -1,4 +1,4 @@
-// Copyright 2015 Andrew 'Diddymus' Rolfe. All rights reserved.
+// Copyright 2019 Andrew 'Diddymus' Rolfe. All rights reserved.
 //
 // Use of this source code is governed by the license in the LICENSE file
 // included with the source code.
@@ -11,7 +11,7 @@ import (
 	"code.wolfmud.org/WolfMUD.git/text"
 )
 
-// Syntax: DROP item
+// Syntax: DROP item...
 func init() {
 	addHandler(drop{}, "DROP")
 }
@@ -25,53 +25,59 @@ func (drop) process(s *state) {
 		return
 	}
 
-	name := s.words[0]
-
-	// Search ourselves for item we want to drop
+	// Check actor has a non-empty inventory
 	from := attr.FindInventory(s.actor)
-
-	// Are we carrying anything at all?
 	if from.Empty() {
 		s.msg.Actor.SendBad("You don't have anything to drop.")
 		return
 	}
 
-	what := from.Search(name)
-
-	// Was item to drop found?
-	if what == nil {
-		s.msg.Actor.SendBad("You have no '", name, "' to drop.")
-		return
-	}
-
-	// Check drop is not vetoed by item, item's current inventory or receiving
-	// inventory
-	for _, t := range []has.Thing{what, s.actor, s.where.Parent()} {
-		for _, vetoes := range attr.FindAllVetoes(t) {
-			if veto := vetoes.Check(s.actor, s.cmd); veto != nil {
-				s.msg.Actor.SendBad(veto.Message())
-				return
-			}
-		}
-	}
-
-	// Get item's proper name
-	name = attr.FindName(what).Name(name)
-
-	// Move the item from our inventory to our location
-	from.Move(what, s.where)
-
-	// As the Thing is now just laying on the ground check if it should register
-	// for clean up
-	attr.FindCleanup(what).Cleanup()
-
-	// Re-enable actions if available
-	attr.FindAction(what).Action()
-
 	who := attr.FindName(s.actor).TheName("Someone")
 	who = text.TitleFirst(who)
 
-	s.msg.Actor.SendGood("You drop ", name, ".")
-	s.msg.Observer.SendInfo(who, " drops ", name, ".")
+	// Find matching items being carried
+nextMatch:
+	for _, match := range MatchAll(
+		s.words,
+		attr.FindInventory(s.actor).Contents(),
+	) {
+		what := match.Thing
+
+		switch {
+		case match.Unknown != "":
+			s.msg.Actor.SendBad("You have no '", match.Unknown, "' to drop.")
+
+		case match.NotEnough != "":
+			s.msg.Actor.SendBad("You don't have that many '", match.NotEnough, "' to drop.")
+
+		default:
+			// Check drop is not vetoed by item, item's current inventory or receiving
+			// inventory
+			for _, t := range []has.Thing{what, s.actor, s.where.Parent()} {
+				for _, vetoes := range attr.FindAllVetoes(t) {
+					if veto := vetoes.Check(s.actor, s.cmd); veto != nil {
+						s.msg.Actor.SendBad(veto.Message())
+						continue nextMatch
+					}
+				}
+			}
+
+			name := attr.FindName(what).Name("something")
+
+			// Move the item from actor's inventory to current location
+			from.Move(what, s.where)
+
+			// As the Thing is now just laying on the ground check if it should
+			// register for clean up
+			attr.FindCleanup(what).Cleanup()
+
+			// Re-enable actions if available
+			attr.FindAction(what).Action()
+
+			s.msg.Actor.SendGood("You drop ", name, ".")
+			s.msg.Observer.SendInfo(who, " drops ", name, ".")
+		}
+	}
+
 	s.ok = true
 }
