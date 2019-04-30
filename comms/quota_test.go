748d31e7a7cf83ad1814d4b1169f4d5f1e1f8f9d
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2019-04-30 18:49:07 +0100

    comms: Replace some 'magic numbers' used in quota tests
    This change replaces some magic numbers such as 4 with maxQuota and 5
    with magicQuota+1. This also allows the tests to pass if the constant
    comms.ringSize is changed.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- comms/quota_test.go
+++ comms/quota_test.go
@@ -19,11 +19,16 @@ import (
 
 // Constants for use with veryify to make tests more readable
 const (
-	UnderQuota = false
-	OverQuota  = true
-	Valid      = false
-	Expired    = true
+	UnderQuota  = false
+	OverQuota   = true
+	Valid       = false
+	Expired     = true
+	IPNotCached = -1
 )
+
+// maxQuota is the maximum number of Quota an IP address can have within a
+// config.Quota.Window period before going over quota.
+var maxQuota = (&comms.Ring{}).Cap()
 
 // verify is a helper that checks the status of the quota cache for an IP
 // address. As quota is not exported we pass in the Query method as a method
@@ -72,7 +77,7 @@ func TestQuota_Query(t *testing.T) {
 	// Test quota count, not over quota and valid
 	for off := 0; off < 256; off++ {
 		ip := "127.0.0." + strconv.Itoa(off)
-		for x := 1; x < 5; x++ {
+		for x := 1; x <= maxQuota; x++ {
 			qq.Quota(ip)
 			verify(t, qq.Query, ip, x, UnderQuota, Valid)
 		}
@@ -84,7 +89,7 @@ func TestQuota_Query(t *testing.T) {
 	// Test quota count, not over quota and expired
 	for off := 0; off < 256; off++ {
 		ip := "127.0.0." + strconv.Itoa(off)
-		verify(t, qq.Query, ip, 4, UnderQuota, Expired)
+		verify(t, qq.Query, ip, maxQuota, UnderQuota, Expired)
 	}
 
 	// Clear quota cache of expired IP addresses
@@ -93,19 +98,19 @@ func TestQuota_Query(t *testing.T) {
 	// Test IP address not in cache
 	for off := 0; off < 256; off++ {
 		ip := "127.0.0." + strconv.Itoa(off)
-		verify(t, qq.Query, ip, -1, UnderQuota, Valid)
+		verify(t, qq.Query, ip, IPNotCached, UnderQuota, Valid)
 	}
 
 	// Test quota count, over quota and valid
 	for off := 0; off < 256; off++ {
 		ip := "127.0.0." + strconv.Itoa(off)
-		for x := 1; x < 5; x++ {
+		for x := 1; x <= maxQuota; x++ {
 			qq.Quota(ip)
 			verify(t, qq.Query, ip, x, UnderQuota, Valid)
 		}
 		// Push over quota
 		qq.Quota(ip)
-		verify(t, qq.Query, ip, 4, OverQuota, Valid)
+		verify(t, qq.Query, ip, maxQuota, OverQuota, Valid)
 	}
 
 }
@@ -128,7 +133,7 @@ func TestQuota_CacheSweep(t *testing.T) {
 	verify(t, qq.Query, ip, 1, UnderQuota, Expired)
 
 	qq.CacheSweep()
-	verify(t, qq.Query, ip, -1, false, false)
+	verify(t, qq.Query, ip, IPNotCached, false, false)
 }
 
 func TestQuota_CacheSweep_stats(t *testing.T) {
@@ -164,7 +169,7 @@ func TestQuota_CacheSweep_stats(t *testing.T) {
 	qq.Quota(ip)
 	qq.Quota(ip)
 	qq.Quota(ip)
-	verify(t, qq.Query, ip, 4, OverQuota, Valid)
+	verify(t, qq.Query, ip, maxQuota, OverQuota, Valid)
 	qq.CacheSweep()
 	want = "quota: 1 entries [0 evicted, 1 over quota, 0 recent]"
 	if strings.TrimSpace(out.String()) != want {
@@ -175,7 +180,7 @@ func TestQuota_CacheSweep_stats(t *testing.T) {
 	time.Sleep(config.Quota.Timeout)
 	out.Reset()
 	qq.CacheSweep()
-	verify(t, qq.Query, ip, -1, false, false)
+	verify(t, qq.Query, ip, IPNotCached, false, false)
 	want = "quota: 1 entries [1 evicted, 0 over quota, 0 recent]"
 	if strings.TrimSpace(out.String()) != want {
 		t.Errorf("CacheSweep stats:\nhave: %s\nwant: %s", out.String(), want)
@@ -190,7 +195,7 @@ func TestQuota_Quota_NoWindow(t *testing.T) {
 	qq := comms.NewQuota()
 	ip := "127.0.0.1"
 	qq.Quota(ip)
-	verify(t, qq.Query, ip, -1, false, false)
+	verify(t, qq.Query, ip, IPNotCached, false, false)
 }
 
 func TestQuota_Quota_Window(t *testing.T) {
@@ -207,7 +212,7 @@ func TestQuota_Quota_Window(t *testing.T) {
 		qq.Quota(ip)
 		time.Sleep(time.Second)
 	}
-	verify(t, qq.Query, ip, 4, UnderQuota, Valid)
+	verify(t, qq.Query, ip, maxQuota, UnderQuota, Valid)
 
 	// Delay long enough for two quota to expire, expired quota will be removed
 	// when we add another.
@@ -224,7 +229,7 @@ func TestQuota_Quota_Window(t *testing.T) {
 	// Check IP address removed from cache
 	time.Sleep(config.Quota.Timeout)
 	qq.CacheSweep()
-	verify(t, qq.Query, ip, -1, false, false)
+	verify(t, qq.Query, ip, IPNotCached, false, false)
 }
 
 func TestQuota_Quota_WithTimeout(t *testing.T) {
@@ -237,22 +242,22 @@ func TestQuota_Quota_WithTimeout(t *testing.T) {
 	ip := "127.0.0.1"
 
 	// Go over quota
-	for x := 0; x < 5; x++ {
+	for x := 1; x <= maxQuota+1; x++ {
 		qq.Quota(ip)
 	}
-	verify(t, qq.Query, ip, 4, OverQuota, Valid)
+	verify(t, qq.Query, ip, maxQuota, OverQuota, Valid)
 
 	// Delay, but not expired
 	time.Sleep(time.Second)
-	verify(t, qq.Query, ip, 4, OverQuota, Valid)
+	verify(t, qq.Query, ip, maxQuota, OverQuota, Valid)
 
 	// Let expire
 	time.Sleep(config.Quota.Timeout)
-	verify(t, qq.Query, ip, 4, OverQuota, Expired)
+	verify(t, qq.Query, ip, maxQuota, OverQuota, Expired)
 
 	// Check IP address removed from cache
 	qq.CacheSweep()
-	verify(t, qq.Query, ip, -1, false, false)
+	verify(t, qq.Query, ip, IPNotCached, false, false)
 }
 
 func TestQuota_Quota_WithoutTimeout(t *testing.T) {
@@ -265,20 +270,20 @@ func TestQuota_Quota_WithoutTimeout(t *testing.T) {
 	ip := "127.0.0.1"
 
 	// Go over quota
-	for x := 0; x < 5; x++ {
+	for x := 1; x <= maxQuota+1; x++ {
 		qq.Quota(ip)
 	}
-	verify(t, qq.Query, ip, 4, OverQuota, Valid)
+	verify(t, qq.Query, ip, maxQuota, OverQuota, Valid)
 
 	// Delay, but not expired
 	time.Sleep(time.Second)
-	verify(t, qq.Query, ip, 4, OverQuota, Valid)
+	verify(t, qq.Query, ip, maxQuota, OverQuota, Valid)
 
 	// Let expire
 	time.Sleep(config.Quota.Window)
-	verify(t, qq.Query, ip, 4, OverQuota, Expired)
+	verify(t, qq.Query, ip, maxQuota, OverQuota, Expired)
 
 	// Check IP address removed from cache
 	qq.CacheSweep()
-	verify(t, qq.Query, ip, -1, false, false)
+	verify(t, qq.Query, ip, IPNotCached, false, false)
 }
