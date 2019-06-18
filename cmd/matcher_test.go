a76e314118475cd6897e0d7ac0493233cd05c549
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2019-06-18 19:16:37 +0100

    cmd: Add slightly pathological (for MatchAll) benchmark case
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- cmd/matcher_test.go
+++ cmd/matcher_test.go
@@ -255,6 +255,7 @@ func BenchmarkMatchAll(b *testing.B) {
 		"frog",
 		"apple frog",
 		"apple frog chalk",
+		"a b c d e f g h i j k l m n o p q r s t u v w x z y",
 	} {
 		b.Run(test, func(b *testing.B) {
 			words := strings.Fields(strings.ToUpper(test))
@@ -334,6 +335,7 @@ func BenchmarkMatch(b *testing.B) {
 		"frog",
 		"apple frog",
 		"apple frog chalk",
+		"a b c d e f g h i j k l m n o p q r s t u v w x z y",
 	} {
 		b.Run(test, func(b *testing.B) {
 			words := strings.Fields(strings.ToUpper(test))
