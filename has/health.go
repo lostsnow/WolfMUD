635ee0e76890614b29dd7a92f329a7828c9d8522
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2020-02-24 20:03:35 +0000

    attr,has: New Health attribute
    Health implements an attribute representing either the state of health
    of a Thing or health bonuses/penalities to be applied to a Thing. If the
    Health attribute is on a Player or Mobile then the values are absolute
    and represent current and maximum health levels of the Player or Mobile.
    If the Health attribute is on anything else the values are relative
    modifiers applied to a Player or Mobile when the Thing is used, worn,
    wielded, eaten, drunk or otherwise applied.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

---
+++ has/health.go
@@ -0,0 +1,20 @@
+// Copyright 2020 Andrew 'Diddymus' Rolfe. All rights reserved.
+//
+// Use of this source code is governed by the license in the LICENSE file
+// included with the source code.
+
+package has
+
+// Health represents the state of health of a Thing.
+//
+// Its default implementation is the attr.Health type.
+type Health interface {
+	Attribute
+
+	// State returns the current and maximum health points.
+	State() (current, maximum int)
+
+	// Adjust increases or decreses the current health points by the given amount.
+	// The new value will be a minimum of 0 and capped at the health maximum.
+	Adjust(int)
+}
