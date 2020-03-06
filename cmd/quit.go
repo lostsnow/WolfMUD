289772d58d45ed0e21a01b74493a1af98ad745fb
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2020-03-06 02:31:10 +0000

    cmd: Also notify participant in QUIT command
    Currently when a player quits a message is sent to any observers.
    However, if the QUIT command is scripted or otherwise used as a
    sub-command there may be a participant that needs to be notified as
    well.
    
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- cmd/quit.go
+++ cmd/quit.go
@@ -18,6 +18,8 @@ func init() {
 
 type quit cmd
 
+const quitMsg = " gives a strangled cry of 'Bye Bye', slowly fades away and is gone."
+
 // The Quit command acts as a hook for processing to be done when a player
 // quits the game.
 func (q quit) process(s *state) {
@@ -44,7 +46,8 @@ func (q quit) process(s *state) {
 	// Remove the player from the world
 	if s.where != nil {
 		who := attr.FindName(s.actor).Name("someone")
-		s.msg.Observer.SendInfo(who, " gives a strangled cry of 'Bye Bye', slowly fades away and is gone.")
+		s.msg.Participant.SendInfo(who, quitMsg)
+		s.msg.Observer.SendInfo(who, quitMsg)
 		s.where.Disable(s.actor)
 		s.where.Remove(s.actor)
 	}
