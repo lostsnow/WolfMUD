8a4d45fdd75735544bb4e4893e576aa1366a8ccb
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2021-05-26 00:27:24 +0100

    proc: Tidy up commands using Find and Msg helpers
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- proc/command.go
+++ proc/command.go
@@ -6,45 +6,47 @@
 package proc
 
 func (s *state) Look() {
+
 	where := World[s.actor.As[Where]]
+
 	switch {
 	case where == nil:
-		s.buff.WriteString("[The Void]\nYou are in a dark void. Around you nothing. No stars, no light, no heat and no sound.")
+		s.Msg("[The Void]\n",
+			"You are in a dark void. Around you nothing.",
+			"No stars, no light, no heat and no sound.")
 	case where.Is&Dark == Dark:
-		s.buff.WriteString("It's too dark to see anything!")
+		s.Msg("It's too dark to see anything!")
 	default:
-		var things string
+		s.Msg("[", where.Name, "]\n", where.Description, "\n\n")
+		mark := s.buff.Len()
 		for _, item := range where.In {
 			if item.Is&Narrative == Narrative {
 				continue
 			}
-			things += "\nYou see " + item.Name + " here."
+			s.Msg("You see ", item.Name, " here.\n")
 		}
-		if len(things) > 0 {
-			things = "\n" + things
+		if s.buff.Len() > mark {
+			s.Msg("\n")
 		}
-		var exits string
+		s.Msg("You see exits:")
 		for dir, text := range DirToName {
 			if where.As[dir] != "" {
-				exits += text + " "
+				s.Msg(" ", text)
 			}
 		}
-		if len(exits) > 0 {
-			exits = "\n\nExits: " + exits
-		}
-		s.buff.WriteString("[" + where.Name + "]\n" +
-			where.Description + things + exits)
 	}
 }
 
 func (s *state) Move() {
+
 	dir := NameToDir[s.cmd]
 	where := World[s.actor.As[Where]]
+
 	switch {
 	case where.As[dir] == "":
-		s.buff.WriteString("You can't go " + DirToName[dir] + "!")
+		s.Msg("You can't go ", DirToName[dir], ".")
 	case World[where.As[dir]] == nil:
-		s.buff.WriteString("Oops! You can't actually go " + DirToName[dir] + ".")
+		s.Msg("Oops! You can't actually go ", DirToName[dir], ".")
 	default:
 		s.actor.As[Where] = where.As[dir]
 		s.Look()
@@ -52,182 +54,132 @@ func (s *state) Move() {
 }
 
 func (s *state) Examine() {
-	var contains string
-	for _, where := range []*Thing{s.actor, World[s.actor.As[Where]]} {
-		for _, what := range where.In {
-			if what.As[Alias] == s.word[0] {
-				if len(what.In) > 0 {
-					contains = " It contains: "
-					for _, item := range what.In {
-						contains += item.Name + ", "
-					}
-					contains = contains[:len(contains)-2] + "."
-				}
-				s.buff.WriteString("You examine " + what.Name + ".\n" + what.Description + contains)
-				return
-			}
+
+	what, _, _ := Find(s.word[0], s.actor, World[s.actor.As[Where]])
+
+	switch {
+	case s.word[0] == "":
+		s.Msg("You go to examine... something?")
+	case what == nil:
+		s.Msg("You see no '", s.word[0], "' to examine.")
+	case len(what.In) == 0:
+		s.Msg("You examine ", what.Name, ".\n", what.Description)
+	default:
+		s.Msg("You examine ", what.Name, ".\n", what.Description)
+		s.Msg(" It contains: ", what.In[0].Name)
+		for _, item := range what.In[1:] {
+			s.Msg(", ", item.Name)
 		}
 	}
-	s.buff.WriteString("You see no '" + s.word[0] + "' to examine.")
 }
 
 func (s *state) Inv() {
-	if len(s.actor.In) == 0 {
-		s.buff.WriteString("You are not carrying anything.")
-		return
+	switch {
+	case len(s.actor.In) == 0:
+		s.Msg("You are not carrying anything.")
+	default:
+		s.Msg("You are carrying:")
+		for _, what := range s.actor.In {
+			s.Msg("\n  ", what.Name)
+		}
 	}
-
-	items := "You are carrying:"
-	for _, what := range s.actor.In {
-		items += "\n  " + what.Name
-	}
-	s.buff.WriteString(items)
 }
 
 func (s *state) Drop() {
-	if s.word[0] == "" {
-		s.buff.WriteString("You go to drop... something?")
-		return
-	}
 
-	for idx, what := range s.actor.In {
-		if what.As[Alias] == s.word[0] {
-			copy(s.actor.In[idx:], s.actor.In[idx+1:])
-			s.actor.In[len(s.actor.In)-1] = nil
-			s.actor.In = s.actor.In[:len(s.actor.In)-1]
+	what, _, idx := Find(s.word[0], s.actor)
 
-			where := World[s.actor.As[Where]]
-			where.In = append(where.In, what)
-			s.buff.WriteString("You drop " + what.Name + ".")
-			return
-		}
+	switch {
+	case s.word[0] == "":
+		s.Msg("You go to drop... something?")
+	case what == nil:
+		s.Msg("You do not have any '", s.word[0], "' to drop.")
+	default:
+		copy(s.actor.In[idx:], s.actor.In[idx+1:])
+		s.actor.In[len(s.actor.In)-1] = nil
+		s.actor.In = s.actor.In[:len(s.actor.In)-1]
+
+		where := World[s.actor.As[Where]]
+		where.In = append(where.In, what)
+		s.Msg("You drop ", what.Name, ".")
 	}
-	s.buff.WriteString("You do not have any '" + s.word[0] + "' to drop.")
 }
 
 func (s *state) Get() {
-	if s.word[0] == "" {
-		s.buff.WriteString("You go to get... something?")
-		return
+
+	what, where, idx := Find(s.word[0], World[s.actor.As[Where]])
+
+	switch {
+	case s.word[0] == "":
+		s.Msg("You go to get... something?")
+	case what == nil:
+		s.Msg("You see no '", s.word[0], "' to get.")
+	case what.Is&Narrative == Narrative:
+		s.Msg("You cannot take ", what.Name, ".")
+	case what.Is&NPC == NPC:
+		s.Msg(what.Name, " does not want to be taken!")
+	default:
+		copy(where.In[idx:], where.In[idx+1:])
+		where.In[len(where.In)-1] = nil
+		where.In = where.In[:len(where.In)-1]
+
+		s.actor.In = append(s.actor.In, what)
+		s.Msg("You get ", what.Name, ".")
 	}
-
-	where := World[s.actor.As[Where]]
-
-	for idx, what := range where.In {
-		if what.As[Alias] == s.word[0] {
-			switch {
-			case what.Is&Narrative == Narrative:
-				s.buff.WriteString("You cannot take " + what.Name + ".")
-			case what.Is&NPC == NPC:
-				s.buff.WriteString(what.Name + " does not want to be taken!")
-			default:
-				copy(where.In[idx:], where.In[idx+1:])
-				where.In[len(where.In)-1] = nil
-				where.In = where.In[:len(where.In)-1]
-
-				s.actor.In = append(s.actor.In, what)
-				s.buff.WriteString("You get " + what.Name + ".")
-			}
-			return
-		}
-	}
-	s.buff.WriteString("You see no '" + s.word[0] + "' to get.")
 }
 
 func (s *state) Take() {
 
+	// Find container, then item in container
+	where, _, _ := Find(s.word[1], s.actor, World[s.actor.As[Where]])
+	what, _, idx := Find(s.word[0], where)
+
 	switch {
 	case s.word[0] == "":
-		s.buff.WriteString("You go to take something from something else...")
-		return
+		s.Msg("You go to take something from something else...")
 	case s.word[1] == "":
-		s.buff.WriteString("You go to take '" + s.word[0] + "' from something...")
-		return
+		s.Msg("You go to take '", s.word[0], "' from something...")
+	case where == nil:
+		s.Msg("You see no '", s.word[1], "' to take anything from.")
+	case what == nil:
+		s.Msg(where.Name, " does not seem to contain '", s.word[0], "'.")
+	default:
+		copy(where.In[idx:], where.In[idx+1:])
+		where.In[len(where.In)-1] = nil
+		where.In = where.In[:len(where.In)-1]
+
+		s.actor.In = append(s.actor.In, what)
+		s.Msg("You take ", what.Name, " from ", where.Name, ".")
 	}
-
-	var where *Thing
-
-	// Find container
-findContainer:
-	for _, inv := range []*Thing{s.actor, World[s.actor.As[Where]]} {
-		for _, item := range inv.In {
-			if item.As[Alias] == s.word[1] {
-				where = item
-				break findContainer
-			}
-		}
-	}
-
-	if where == nil {
-		s.buff.WriteString("You see no '" + s.word[1] + "' to take anything from.")
-		return
-	}
-
-	// Find item in container
-	for idx, what := range where.In {
-		if what.As[Alias] == s.word[0] {
-
-			// Remove item from container
-			copy(where.In[idx:], where.In[idx+1:])
-			where.In[len(where.In)-1] = nil
-			where.In = where.In[:len(where.In)-1]
-
-			// Give to player
-			s.actor.In = append(s.actor.In, what)
-
-			s.buff.WriteString("You take " + what.Name + " from " + where.Name + ".")
-			return
-		}
-	}
-
-	s.buff.WriteString(where.Name + " does not seem to contain '" + s.word[0] + "'.")
 }
 
 func (s *state) Put() {
 
+	// Find container, find item (must be carried)
+	where, _, _ := Find(s.word[1], s.actor, World[s.actor.As[Where]])
+	what, _, idx := Find(s.word[0], s.actor)
+
 	switch {
 	case s.word[0] == "":
-		s.buff.WriteString("You go to put something into something else...")
-		return
+		s.Msg("You go to put something into something else...")
+	case s.word[1] == "" && what == nil:
+		s.Msg("You go to put '", s.word[0], "' into something...")
 	case s.word[1] == "":
-		s.buff.WriteString("You go to put '" + s.word[0] + "' into something...")
-		return
+		s.Msg("You go to put ", what.Name, " into something...")
+	case where == nil && what == nil:
+		s.Msg("You see no '", s.word[1], "' to put anything in.")
+	case where == nil:
+		s.Msg("You see no '", s.word[1], "' to put ", what.Name, " in.")
+	case where.Is&NPC == NPC:
+		s.Msg("Taxidermist are we?")
+	case what == nil:
+		s.Msg("You have no '", s.word[0], "' to put into ", where.Name+".")
+	default:
+		copy(s.actor.In[idx:], s.actor.In[idx+1:])
+		s.actor.In[len(s.actor.In)-1] = nil
+		s.actor.In = s.actor.In[:len(s.actor.In)-1]
+
+		where.In = append(where.In, what)
+		s.Msg("You put ", what.Name, " into ", where.Name, ".")
 	}
-
-	var where *Thing
-
-	// Find container
-findContainer:
-	for _, inv := range []*Thing{s.actor, World[s.actor.As[Where]]} {
-		for _, item := range inv.In {
-			if item.As[Alias] == s.word[1] {
-				where = item
-				break findContainer
-			}
-		}
-	}
-
-	if where == nil {
-		s.buff.WriteString("You have no '" + s.word[1] + "' to put anything in.")
-		return
-	}
-
-	// Find item (must be carried)
-	for idx, what := range s.actor.In {
-		if what.As[Alias] == s.word[0] {
-
-			// Remove item from player
-			copy(s.actor.In[idx:], s.actor.In[idx+1:])
-			s.actor.In[len(s.actor.In)-1] = nil
-			s.actor.In = s.actor.In[:len(s.actor.In)-1]
-
-			// Put into container
-			where.In = append(where.In, what)
-
-			s.buff.WriteString("You put " + what.Name + " into " + where.Name + ".")
-			return
-		}
-	}
-
-	s.buff.WriteString("You do not have '" + s.word[0] + "' to put into "+where.Name+".")
 }
