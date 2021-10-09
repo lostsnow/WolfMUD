lostsnow/WolfMUD

commit a421285f13cb48941aa685025ba6d9c5e3d612c8

core: Thing.Schedule cancel event when it fires to clean up state
Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

core/thing.go
@@ -522,6 +522,7 @@ func (t *Thing) Schedule(event eventKey) {
 	t.Int[idx+DueAtOffset] = time.Now().Add(wait).UnixNano()
 	t.Event[event] = time.AfterFunc(
 		wait, func() {
+			t.Cancel(event)
 			NewState(t).Parse(eventCommands[event])
 		},
 	)

commit 1a78c2825f57c1af3c5fd238b4dee314c2d3f288

core: Fix data race in *Thing.Schedule event handling
A data race was introduced in commit a421285 "core: Thing.Schedule
cancel event when it fires to clean up state".

The t.Cancel(event) call is made via time.AfterFunc which does not hold
the state.BWL causing a data race.

This commit removes the t.Cancel(event) from the time.AfterFunc call.
Instead the Cancel call is placed at the start of each specific event
handler which is called with the state.BWL held.

Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

core/command.go
@@ -872,6 +872,7 @@ func (s *state) Say() {
 }
 
 func (s *state) Action() {
+	s.actor.Cancel(Action)
 	l := len(s.actor.Any[OnAction])
 	if l == 0 {
 		return
@@ -977,6 +978,7 @@ func radius(size int, where *Thing) [][]*Thing {
 }
 
 func (s *state) Reset() {
+	s.actor.Cancel(Reset)
 
 	// If actor should wait and has out of play items don't reset
 	if s.actor.Is&Wait == Wait && len(s.actor.Out) > 0 {
@@ -1056,6 +1058,7 @@ func (s *state) Junk() {
 }
 
 func (s *state) Cleanup() {
+	s.actor.Cancel(Cleanup)
 	defer s.actor.Junk()
 
 	where := s.actor.Ref[Where]
@@ -1078,6 +1081,7 @@ func (s *state) Cleanup() {
 // be set via Thing.As[TriggerType]. The item can be in or out of play at the
 // time.
 func (s *state) Trigger() {
+	s.actor.Cancel(Trigger)
 
 	switch s.actor.As[TriggerType] {
 	case "BLOCKER":

core/thing.go
@@ -762,7 +762,6 @@ func (t *Thing) Schedule(event eventKey) {
 	t.Int[idx+DueAtOffset] = time.Now().Add(wait).UnixNano()
 	t.Event[event] = time.AfterFunc(
 		wait, func() {
-			t.Cancel(event)
 			NewState(t).Parse(eventCommands[event])
 		},
 	)