fa3410e08e9d123a1faf0bd65c34c2790ef99433
Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>
2022-04-30 20:57:26 +0100

    core: Fix comment in version.go due to package rename
    Signed-off-by: Andrew 'Diddymus' Rolfe <diddymus@wolfmud.org>

--- core/version.go
+++ core/version.go
@@ -7,7 +7,7 @@ package core
 
 // commit is overridden at compile time using:
 //
-//	-ldflags "-X code.wolfmud.org/WolfMUD.git/cmd.commit=id"
+//	-ldflags "-X code.wolfmud.org/WolfMUD.git/core.commit=id"
 //
 // Where id is the git commit returned by git describe HEAD when compiling,
 // optionally followed by '-dirty' if the worktree has uncomitted changes. For
